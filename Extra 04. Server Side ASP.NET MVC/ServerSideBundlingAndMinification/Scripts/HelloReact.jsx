473c58da11e57df53e8c628a3ba96e542d4eb9ca
Rick Beerendonk <rick@beerendonk.com>
2014-09-19 00:03:46 +0200

    Add "Extra 04. Server Side ASP.NET MVC"

---
+++ Extra 04. Server Side ASP.NET MVC/ServerSideBundlingAndMinification/Scripts/HelloReact.jsx
@@ -0,0 +1,14 @@
+﻿/** @jsx React.DOM */
+
+var HelloReact = React.createClass({
+    render: function () {
+        return (
+            <h1>Hello React!</h1>
+        );
+    }
+});
+
+React.renderComponent(
+    <HelloReact />,
+    document.getElementById('app')
+);
---
+++ Extra 04. Server Side ASP.NET MVC/ServerSideComponentRendering/Scripts/ClickMe.jsx
@@ -0,0 +1,15 @@
+﻿/** @jsx React.DOM */
+
+var ClickMe = React.createClass({
+    getInitialState: function () {
+        return {count: 0};
+    },
+    onClick: function (e) {
+        this.setState({count: this.state.count + 1});
+    },
+    render: function () {
+        return <a onClick={this.onClick}>
+            {'This link has been clicked ' + this.state.count + ' times'}
+            </a>;
+    }
+});
