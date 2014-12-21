82173b2cdf97eb0a2d329d19bf5c62039c8bad0b
Rick Beerendonk <rick@beerendonk.com>
2014-12-21 15:10:51 +0100

    Update to Om 0.8.0-beta4 and React 0.12.2

--- 02. Component/om/project.clj
+++ 02. Component/om/project.clj
@@ -3,8 +3,8 @@
   :url "http://om.beerendonk.com"
 
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-2322"]
-                 [om "0.7.3"]]
+                 [org.clojure/clojurescript "0.0-2371"]
+                 [om "0.8.0-beta4"]]
 
   :plugins [[lein-cljsbuild "1.0.4-SNAPSHOT"]]
 
