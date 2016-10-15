b1fbe4a9f68a6ab2c5b92ae1a27d394360106046
Rick Beerendonk <rick@beerendonk.com>
2016-10-15 13:56:02 +0200

    Add fetch examples

---
+++ 10 - Fetch/jsx/app.jsx
@@ -0,0 +1,46 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2016 Rick Beerendonk   !*/
+
+/* global React, ReactDOM */
+/* eslint react/prop-types:"off" */
+
+var Post = React.createClass({
+  render: function () {
+    return (
+      <li>{this.props.title}</li>
+    );
+  }
+});
+
+var Posts = React.createClass({
+  getInitialState: function () {
+    return { posts: [] };
+  },
+  componentDidMount() {
+    var self = this;
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+      if (this.readyState == XMLHttpRequest.DONE && this.status == 200) {
+        self.setState({ posts: JSON.parse(xhr.responseText) });
+      }
+    };
+    xhr.open('GET', 'http://jsonplaceholder.typicode.com/posts', true);
+    xhr.send();
+  },
+  render() {
+    return (
+      <ul>{
+        this.state.posts.map(
+          function (post, i) {
+            return <Post key={i} title={post.title} />;
+          }
+        )
+      }</ul>
+    );
+  }
+});
+
+ReactDOM.render(
+  <Posts />,
+  document.getElementById('app')
+);
