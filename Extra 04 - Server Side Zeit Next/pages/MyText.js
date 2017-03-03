d6ab7d2efffc6a12f5b140e2441aaa9daa1b72eb
Rick Beerendonk <rick@beerendonk.com>
2017-03-03 13:20:37 +0100

    Add Zeit Next example

---
+++ Extra 04 - Server Side Zeit Next/pages/MyText.js
@@ -0,0 +1,3 @@
+import React from 'react'
+
+export default ({text}) => <i>{text}</i>
---
+++ Extra 04 - Server Side Zeit Next/pages/index.js
@@ -0,0 +1,4 @@
+import React from 'react';
+import MyText from './MyText.js';
+
+export default () => <h1>Hello <MyText text="world" />!</h1>
