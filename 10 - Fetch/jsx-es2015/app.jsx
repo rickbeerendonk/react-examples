7ed60c9c5989b9645b7bdb79e49433f0b06d6883
Rick Beerendonk <rick@beerendonk.com>
2017-03-15 11:38:43 -0500

    Handle http errors in fetch.

--- 10 - Fetch/jsx-es2015/app.jsx
+++ 10 - Fetch/jsx-es2015/app.jsx
@@ -9,20 +9,27 @@ let Post = ({title}) => <li>{title}</li>;
 class Posts extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { posts: [] };
+    this.state = { posts: [], error: 'test' };
   }
   componentDidMount() {
-    fetch('http://jsonplaceholder.typicode.com/posts').then(response => {
-      response.json().then(json => {
-        this.setState({ posts: json });
+    fetch('http://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        if (!response.ok) {
+          throw Error(response.statusText);
+        }
+        return response.json()
       })
-    })
+      .then(json => { this.setState({ posts: json }); })
+      .catch(error => { this.setState({ error: error.message }) });
   }
   render() {
     return (
-      <ul>{
-        this.state.posts.map((post, i) => <Post key={i} title={post.title} />)
-      }</ul>
+      <div>
+        <div style={{color: 'red'}}>{this.state.error}</div>
+        <ul>{
+          this.state.posts.map((post, i) => <Post key={i} title={post.title} />)
+        }</ul>
+      </div>
     );
   }
 }
