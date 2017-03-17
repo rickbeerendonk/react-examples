98bbbcdeb1779bb8f5c9aab3d9a9ef1544ccdd8c
Rick Beerendonk <rick@beerendonk.com>
2017-03-17 17:10:51 -0500

    Upgrade to React.NET 3.0.1
    Signed-off-by: Rick Beerendonk <rick@beerendonk.com>

--- Extra 03 - Server Side ASP.NET MVC/ServerSideComponentRendering/Scripts/ClickMe.jsx
+++ Extra 03 - Server Side ASP.NET MVC/ServerSideComponentRendering/Scripts/ClickMe.jsx
@@ -5,13 +5,15 @@ class ClickMe extends React.Component {
     constructor(props) {
         super(props);
         this.state = {count: 0};
+
+        this.onClick = this.onClick.bind(this);
     }
     onClick() {
         this.setState(prevState => ({count: prevState.count + 1}));
     }
     render() {
         return (
-            <a onClick={this.onClick.bind(this)}>
+            <a onClick={this.onClick}>
                 {'This link has been clicked ' + this.state.count + ' times'}
             </a>
         );
