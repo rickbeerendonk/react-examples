95234b1f86237f85af4637c26d0246a90dec0f95
Rick Beerendonk <rick@beerendonk.com>
2017-04-24 15:46:41 +0200

    Input component with OnChange just like in HTML (fires onBlur when there is a real change).

---
+++ 11 - Input - b. Browser OnChange/jsx-es2015/app.jsx
@@ -0,0 +1,69 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2016 Rick Beerendonk   !*/
+
+/* global React, ReactDOM */
+/* eslint react/prop-types:"off" */
+
+class MyInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.browserChanged = this.browserChanged.bind(this);
+  }
+  browserChanged(e) {
+    this.props.onBrowserChanged(e);
+  }
+  componentDidMount() {
+    this.myInput.addEventListener('change', this.browserChanged);
+  }
+  componentWillUnmount() {
+    this.myInput.removeEventListener('change', this.browserChanged);
+  }
+  render() {
+    return (
+      <input {...this.props} ref={(ref) => this.myInput = ref}>{this.props.children}</input>
+    );
+  }
+}
+
+class HelloMessage extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: 'World' };
+
+    // Bind all non-react methods to this.
+    this.onBrowserChanged = this.onBrowserChanged.bind(this);
+    this.onChange = this.onChange.bind(this);
+  }
+  onBrowserChanged(e) {
+    alert(e.target.value);
+  }
+  onChange(e) {
+    this.setState({ name: e.target.value });
+  }
+  render() {
+    return (
+      <div>
+        <div>
+          Not controlled by React:
+          <MyInput
+            onChange={this.onChange}
+            onBrowserChanged={this.onBrowserChanged}
+            defaultValue={this.state.name} />
+        </div>
+
+        <div>
+          Controlled by React:
+          <MyInput 
+            onChange={this.onChange} 
+            onBrowserChanged={this.onBrowserChanged}
+            value={this.state.name} />
+        </div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <HelloMessage />,
+  document.getElementById('app')
+);
