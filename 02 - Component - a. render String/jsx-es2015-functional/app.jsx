5e82e6029092b5795d6cdad845ee583cddeb22fa
Rick Beerendonk <rick@beerendonk.com>
2017-05-20 16:11:51 +0200

    Add render string example

---
+++ 02 - Component - a. render String/jsx-es2015-functional/app.jsx
@@ -0,0 +1,13 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2016 Rick Beerendonk   !*/
+
+/* global React, ReactDOM */
+
+const Greeting = () => (
+  'Hello World!'
+);
+
+ReactDOM.render(
+  <Greeting />,
+  document.getElementById('app')
+);
