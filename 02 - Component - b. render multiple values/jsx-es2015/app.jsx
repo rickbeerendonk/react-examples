00fc04f340331f9f61ea3d100f5807c0611aa0fb
Rick Beerendonk <rick@beerendonk.com>
2017-05-20 16:20:27 +0200

    Add render multiple values example

---
+++ 02 - Component - b. render multiple values/jsx-es2015/app.jsx
@@ -0,0 +1,19 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2016 Rick Beerendonk   !*/
+
+/* global React, ReactDOM */
+
+class Greeting extends React.Component {
+  render() {
+    return [
+      <h1>Hello World!</h1>,
+      'What do you think about React?',
+      <div>It looks amazing.</div>
+    ];
+  }
+}
+
+ReactDOM.render(
+  <Greeting />,
+  document.getElementById('app')
+);
