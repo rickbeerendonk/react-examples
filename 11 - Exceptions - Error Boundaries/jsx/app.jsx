dc3b17ba3e5a9ef1b33cf4d03bfa35fcdbd6ffaa
Rick Beerendonk <rick@beerendonk.com>
2017-08-03 22:56:12 +0200

    Remove the "ES2015" name from the JSX examples.

---
+++ 11 - Exceptions - Error Boundaries/jsx/app.jsx
@@ -0,0 +1,62 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2016 Rick Beerendonk   !*/
+
+/* global React, ReactDOM */
+
+class GreetingEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: 'World' };
+
+    // Bind all non-react methods to this.
+    this.onChange = this.onChange.bind(this);
+  }
+  onChange(e) {
+    const value = e.target.value;
+    this.setState(() => ({ name: value }));
+  }  
+  shouldComponentUpdate(nextProps, nextState) {
+    if (nextState.name === 'Error') {
+      throw new Error('Error');
+    }
+
+    return true;
+  }
+  render() {
+    if (this.state.name === 'renderException') {
+      throw new Error('render exception');
+    }
+
+    return [
+      <input onChange={this.onChange} value={this.state.name} />,
+      <h1>Hello {this.state.name}!</h1>
+    ];
+  }
+}
+
+class ErrorBoundary extends React.Component {
+  state = {error: null, info: null};
+  componentDidCatch(error, info) {
+    this.setState({error});
+  }
+  render() {
+    if (this.state.error) {
+      return (
+        <div style={{color: 'red'}}>
+          <h1>{this.state.error.message}</h1>
+          <p>{this.state.error.stack}</p>
+          <h1>Info</h1>
+          <p>{this.state.info}</p>
+        </div>
+      );
+    }
+    return this.props.children;
+  }
+}
+
+ReactDOM.render(
+  <ErrorBoundary>
+    <GreetingEditor />
+  </ErrorBoundary>,
+  document.getElementById('app')
+);
