7786a44bb065ac2fe7214781c28a6254231a210b
Rick Beerendonk <rick@beerendonk.com>
2018-01-10 12:34:31 -0500

    Event Pool examples

---
+++ 04 - Events - a. Pool/jsx-functional/app.jsx
@@ -0,0 +1,29 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+/* global React, ReactDOM */
+
+const Greeting = () => { 
+  let changed = (e) => {
+    // Events are pooled. To prevent properties from resetting, remove the
+    // event from the pool by calling event.persist(). See error in console.
+    e.persist();
+
+    const showHandled = () => {
+      alert(`Event handled.
+Target: ${e.target}
+Value: ${e.target.value}`)
+    };
+
+    setTimeout(showHandled, 2000);
+  };
+
+  return (
+    <input onChange={changed} value='Hello World!' />
+  )
+};
+
+ReactDOM.render(
+  <Greeting />,
+  document.getElementById('app')
+);
