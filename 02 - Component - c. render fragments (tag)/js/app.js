a4c41632ef56d4e8c13acb6806ce0506dea31e86
Rick Beerendonk <rick@beerendonk.com>
2018-01-17 13:41:46 -0500

    Add fragments tag examples

---
+++ 02 - Component - c. render fragments (tag)/js/app.js
@@ -0,0 +1,21 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+/* global React, ReactDOM */
+
+class Greeting extends React.Component {
+  render() {
+    return React.createElement(
+      React.Fragment, 
+      null,
+      React.createElement('h1', null, 'Hello World!'),
+      'What do you think about React?',
+      React.createElement('div', null, 'It looks amazing.')
+    );
+  }
+}
+
+ReactDOM.render(
+  React.createElement(Greeting),
+  document.getElementById('app')
+);
---
+++ 02 - Component - c. render fragments (tag)/jsx/app.jsx
@@ -0,0 +1,21 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+/* global React, ReactDOM */
+
+class Greeting extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h1>Hello World!</h1>
+        What do you think about React?
+        <div>It looks amazing.</div>
+      </React.Fragment>
+    );
+  }
+}
+
+ReactDOM.render(
+  <Greeting />,
+  document.getElementById('app')
+);
