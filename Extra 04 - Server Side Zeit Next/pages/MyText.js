bcde25eadce2aebc87e8ebe6ae174502ccae2a64
Rick Beerendonk <rick@beerendonk.com>
2018-01-25 03:59:05 +0100

    Update version (due to GitHub security vulnerability alert)

--- Extra 04 - Server Side Zeit Next/pages/MyText.js
+++ Extra 04 - Server Side Zeit Next/pages/MyText.js
@@ -1,3 +1,6 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2017 Rick Beerendonk   !*/
+
 import React from 'react'
 
 /* eslint react/prop-types:"off" */
--- Extra 04 - Server Side Zeit Next/pages/index.js
+++ Extra 04 - Server Side Zeit Next/pages/index.js
@@ -1,3 +1,6 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2017 Rick Beerendonk   !*/
+
 import React from 'react';
 import MyText from './MyText.js';
 
