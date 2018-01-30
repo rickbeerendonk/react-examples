210cd4950c608ed9f2bb9d38705702baec707b69
Rick Beerendonk <rick@beerendonk.com>
2018-01-30 08:08:07 +0100

    Formatted documents.

--- Setup 02 - Babel/site/src/app.jsx
+++ Setup 02 - Babel/site/src/app.jsx
@@ -10,11 +10,11 @@ class HelloReact extends React.Component {
   render() {
     return (
       <h1>Hello React!</h1>
-      );
+    );
   }
 }
 
 ReactDOM.render(
   <HelloReact />,
   document.getElementById('app')
-  );
+);
