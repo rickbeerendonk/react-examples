e78056d644c145c89cfb390767014164ca94bd2e
Rick Beerendonk <rick@beerendonk.com>
2018-01-31 14:58:49 +0100

    Add Component examples for systemjs Typescript.

---
+++ 02 - Component/systemjs/typescript/jsx-functional/Greeting.tsx
@@ -0,0 +1,10 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+import React from 'react';
+
+const Greeting = () => (
+  <h1>Hello World!</h1>
+);
+
+export default Greeting;
---
+++ 02 - Component/systemjs/typescript/jsx/Greeting.tsx
@@ -0,0 +1,12 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+import React from 'react';
+
+export default class Greeting extends React.Component<undefined, undefined> {
+  render() {
+    return (
+      <h1>Hello World!</h1>
+    );
+  }
+}
