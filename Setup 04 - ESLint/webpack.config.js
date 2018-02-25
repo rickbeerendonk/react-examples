dcd1b49885994effa87276983f732bad11900a18
Rick Beerendonk <rick@beerendonk.com>
2018-02-25 15:41:09 +0100

    Upgrade to Webpack 4.

--- Setup 04 - ESLint/webpack.config.js
+++ Setup 04 - ESLint/webpack.config.js
@@ -1,3 +1,10 @@
+/* global __dirname, module, require */
+
+// Webpack 4 workaround for eslint-loader 1.9.0 build bug.
+// Pull request to fix: https://github.com/webpack-contrib/eslint-loader/pull/212
+// (TODO: Remove in future versions):
+var webpack = require('webpack');
+
 module.exports = {
   context: __dirname + '/site/src',
   entry: './app.jsx',
@@ -20,5 +27,11 @@ module.exports = {
         exclude: /node_modules/
       }
     ]
-  }
+  },
+  plugins: [
+    // Webpack 4 workaround for eslint-loader 1.9.0 build bug.
+    // Pull request to fix: https://github.com/webpack-contrib/eslint-loader/pull/212
+    // (TODO: Remove in future versions):
+    new webpack.LoaderOptionsPlugin({ options: {} })
+  ]
 };
