6a017116534c9e234120da5bbc326b987d1f96c3
Rick Beerendonk <rick@beerendonk.com>
2018-02-26 15:28:13 +0100

    Update eslint-loader to Webpack 4 compatible version.

--- Setup 04 - ESLint/webpack.config.js
+++ Setup 04 - ESLint/webpack.config.js
@@ -1,9 +1,4 @@
-/* global __dirname, module, require */
-
-// Webpack 4 workaround for eslint-loader 1.9.0 build bug.
-// Pull request to fix: https://github.com/webpack-contrib/eslint-loader/pull/212
-// (TODO: Remove in future versions):
-var webpack = require('webpack');
+/* global __dirname, module */
 
 module.exports = {
   context: __dirname + '/site/src',
@@ -27,11 +22,5 @@ module.exports = {
         exclude: /node_modules/
       }
     ]
-  },
-  plugins: [
-    // Webpack 4 workaround for eslint-loader 1.9.0 build bug.
-    // Pull request to fix: https://github.com/webpack-contrib/eslint-loader/pull/212
-    // (TODO: Remove in future versions):
-    new webpack.LoaderOptionsPlugin({ options: {} })
-  ]
+  }
 };
