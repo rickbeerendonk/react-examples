cfb8ae17c9441bfbe2ae9b5c965447475a79b267
Rick Beerendonk <rick@beerendonk.com>
2018-03-27 17:39:17 -0500

    Add Higher-Order Component example (runtime-jsx-only).

---
+++ 18 - Higher-Order Components/runtime/javascript/jsx/app.jsx
@@ -0,0 +1,41 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+/* global React, ReactDOM */
+/* eslint react/prop-types:"off", react/display-name:"off" */
+
+// HOC
+function withExtras(WrappedComponent) {
+  return class extends React.Component {
+    render() {
+      // Filter out extra props that are specific to this HOC and shouldn't be
+      // passed through
+      const { extraColor, ...passThroughProps } = this.props;
+
+      return (
+        <div style={{ color: extraColor }}>
+          <WrappedComponent {...passThroughProps} />
+        </div>
+      );
+    }
+  }
+}
+
+const Greeting = ({name}) => (
+  <h1>Hello {name}!</h1>
+);
+
+const GreetingWithExtras = withExtras(Greeting);
+
+class App extends React.Component {
+  render() {
+    return (
+      <GreetingWithExtras extraColor="red" name="World" />
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
