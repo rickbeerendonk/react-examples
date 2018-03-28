fd8df42e3ae29d44923a7f611b3fd82b6e7e5d7c
Rick Beerendonk <rick@beerendonk.com>
2018-03-28 08:32:46 -0500

    Add HOC - DOM solution with forwardRef examples (runtime-only).

---
+++ 18 - Higher-Order Components - b. DOM solution with forwardRef (vNEXT)/runtime/javascript/js/app.js
@@ -0,0 +1,43 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+/* global React, ReactDOM */
+/* eslint react/prop-types:"off", react/display-name:"off" */
+
+// Higher-Order Component
+function withExtras(WrappedComponent) {
+  class WithExtras extends React.Component {
+    render() {
+      return (
+        React.createElement(WrappedComponent, this.props)
+      );
+    }
+  }
+
+  return React.forwardRef((props, ref) =>
+    React.createElement(WithExtras, Object.assign({}, props, {forwardedRef: ref}))
+  );
+}
+const Greeting = (props) => (
+  React.createElement('h1', { ref: props.forwardedRef }, 'Hello ', props.name, '!')
+);
+
+const GreetingWithExtras = withExtras(Greeting);
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.myGreeting = React.createRef();
+  }
+  componentDidMount() {
+    this.myGreeting.current.innerHTML = 'Hi <i>React</i>!!!';
+  }
+  render() {
+    return React.createElement(GreetingWithExtras, { ref: this.myGreeting, name: 'World <- Should be replaced'});
+  }
+}
+
+ReactDOM.render(
+  React.createElement(App),
+  document.getElementById('app')
+);
---
+++ 18 - Higher-Order Components - b. DOM solution with forwardRef (vNEXT)/runtime/javascript/jsx-functional/app.jsx
@@ -0,0 +1,40 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+/* global React, ReactDOM */
+/* eslint react/prop-types:"off", react/display-name:"off" */
+
+// Higher-Order Component
+function withExtras(WrappedComponent) {
+  const WithExtras = props => <WrappedComponent {...props} />;
+
+  return React.forwardRef((props, ref) =>
+    <WithExtras {...props} forwardedRef={ref} />
+  );
+}
+
+const Greeting = (props) => (
+  <h1 ref={props.forwardedRef}>Hello {props.name}!</h1>
+);
+
+const GreetingWithExtras = withExtras(Greeting);
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.myGreeting = React.createRef();
+  }
+  componentDidMount() {
+    this.myGreeting.current.innerHTML = 'Hi <i>React</i>!!!';
+  }
+  render() {
+    return (
+      <GreetingWithExtras name="World <- Should be replaced" ref={this.myGreeting} />
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
