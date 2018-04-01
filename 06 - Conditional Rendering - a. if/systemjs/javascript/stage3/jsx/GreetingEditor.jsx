ca207aa99104dabbf453d4193d787c32b3a15fe4
Rick Beerendonk <rick@beerendonk.com>
2018-04-01 09:55:48 +0200

    Add Conditional Rendering - If example.

---
+++ 06 - Conditional Rendering - a. if/systemjs/javascript/stage3/jsx/GreetingEditor.jsx
@@ -0,0 +1,27 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+/* eslint react/prop-types:"off" */
+
+import React from 'react';
+
+export default class GreetingEditor extends React.Component {
+  // Proposal: https://github.com/tc39/proposal-class-fields
+  // Support: http://kangax.github.io/compat-table/esnext/#test-class_fields
+  state = { name: 'World' };
+  onChange = (e) => {
+    this.setState({ name: e.target.value });
+  }
+
+  render() {
+    return (
+      <div>
+        <input onChange={this.onChange} value={this.state.name} />
+        {
+          // Only render when a name is truthy:
+          this.state.name && <h1>Hello {this.state.name}!</h1>
+        }
+      </div>
+    );
+  }
+}
