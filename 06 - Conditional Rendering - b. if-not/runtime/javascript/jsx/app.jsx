5d8681c7ba54f39650257ab119c10feeeffce5ee
Rick Beerendonk <rick@beerendonk.com>
2018-04-01 10:55:09 +0200

    Add Conditional Rendering - If-not example.

---
+++ 06 - Conditional Rendering - b. if-not/runtime/javascript/jsx/app.jsx
@@ -0,0 +1,32 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2016 Rick Beerendonk   !*/
+
+/* global React, ReactDOM */
+
+class GreetingEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: 'World' };
+
+    this.onChange = this.onChange.bind(this);
+  }
+  onChange(e) {
+    this.setState({ name: e.target.value });
+  }
+  render() {
+    return (
+      <div>
+        <input onChange={this.onChange} value={this.state.name} />
+        {
+          // Only render when a not-name is falsy:
+          !this.state.name || <h1>Hello {this.state.name}!</h1>
+        }
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(
+  <GreetingEditor />,
+  document.getElementById('app')
+);
---
+++ 06 - Conditional Rendering - b. if-not/systemjs/javascript/jsx/GreetingEditor.jsx
@@ -0,0 +1,29 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+/* eslint react/prop-types:"off" */
+
+import React from 'react';
+
+export default class GreetingEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: 'World' };
+
+    this.onChange = this.onChange.bind(this);
+  }
+  onChange(e) {
+    this.setState({ name: e.target.value });
+  }
+  render() {
+    return (
+      <div>
+        <input onChange={this.onChange} value={this.state.name} />
+        {
+          // Only render when a not-name is falsy:
+          !this.state.name || <h1>Hello {this.state.name}!</h1>
+        }
+      </div>
+    );
+  }
+}
