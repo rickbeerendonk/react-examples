efb7e6ecf1f91ef2c693f17b1edfa9af60a34056
Rick Beerendonk <rick@beerendonk.com>
2018-04-23 20:41:05 +0200

    Add Render Props - Children example.

---
+++ 20 - Render Props - a. Children/runtime/javascript/stage3/jsx/app.jsx
@@ -0,0 +1,59 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+/* global React, ReactDOM */
+/* eslint react/prop-types:"off" */
+
+// Component with render prop
+class MouseOver extends React.Component {
+  // Proposal: https://github.com/tc39/proposal-class-fields
+  // Support: http://kangax.github.io/compat-table/esnext/#test-class_fields
+  state = { mouseIsOver: false };
+  onMouseEnter = () => {
+    this.setState({ mouseIsOver: true });
+  }
+  onMouseLeave = () => {
+    this.setState({ mouseIsOver: false });
+  }
+  
+  render() {
+    return (
+      <div onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave}>
+        {/*
+          Call the props.render function with what this component knows about the mouse
+          (this.state can be removed if state contains more than 1 property):
+        */}
+        {this.props.children(this.state.mouseIsOver)}
+      </div>
+    );
+  }
+}
+
+class Greeting extends React.Component {
+  render() {
+    return (
+      <h1 style={this.props.highlight ? {color: 'red'} : null}>Hello {this.props.name}!</h1>
+    );
+  }
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <MouseOver>
+          {mouseIsOver => (<Greeting name="First" highlight={mouseIsOver} />)}
+        </MouseOver>
+        <Greeting name="Second" />
+        <MouseOver>
+          {mouseIsOver => (<h2>Subtitle{mouseIsOver && ' (with mouse)'}</h2>)}
+        </MouseOver>
+      </React.Fragment>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
---
+++ 20 - Render Props - a. Children/systemjs/javascript/jsx/App.jsx
@@ -0,0 +1,25 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+/* eslint react/prop-types:"off" */
+
+import React from 'react';
+
+import Greeting from './Greeting';
+import MouseOver from './MouseOver';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <MouseOver>
+          {mouseIsOver => (<Greeting name="First" highlight={mouseIsOver} />)}
+        </MouseOver>
+        <Greeting name="Second" />
+        <MouseOver>
+          {mouseIsOver => (<h2>Subtitle{mouseIsOver && ' (with mouse)'}</h2>)}
+        </MouseOver>
+      </React.Fragment>
+    );
+  }
+}
