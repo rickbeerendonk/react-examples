fdc33a64e3483b62f98659d395231b4fcdd2a8ed
Rick Beerendonk <rick@beerendonk.com>
2018-04-23 19:07:59 +0200

    Add Render Props example (runtime - jsx only).

---
+++ 20 - Render Props/runtime/javascript/jsx/app.jsx
@@ -0,0 +1,54 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+/* global React, ReactDOM */
+/* eslint react/prop-types:"off", react/display-name:"off" */
+
+// Component with render prop
+class MouseOver extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { mouseIsOver: false };
+
+    this.onMouseEnter = this.onMouseEnter.bind(this);
+    this.onMouseLeave = this.onMouseLeave.bind(this);
+  }
+  onMouseEnter() {
+    this.setState({ mouseIsOver: true });
+  }
+  onMouseLeave() {
+    this.setState({ mouseIsOver: false });
+  }
+  render() {
+    return (
+      <div onMouseEnter={this.onMouseEnter} onMouseLeave={this.onMouseLeave}>
+        {/*
+          Call the props.render function with what this component knows about the mouse
+          (this.state can be removed if state contains more than 1 property):
+        */}
+        {this.props.render(this.state.mouseIsOver)}
+      </div>
+    );
+  }
+}
+
+const Greeting = ({name, highlight}) => (
+  <h1 style={highlight ? {color: 'red'} : null}>Hello {name}!</h1>
+);
+
+class App extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <MouseOver render={mouseIsOver => (<Greeting name="First" highlight={mouseIsOver} />)} />
+        <Greeting name="Second" />
+        <MouseOver render={mouseIsOver => (<h2 style={mouseIsOver ? {color: 'blue'} : null}>Subtitle</h2>)} />
+      </React.Fragment>
+    );
+  }
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('app')
+);
