887b8fa2af3011ef15f35c8038553dedea59fa54
Rick Beerendonk <rick@beerendonk.com>
2018-06-17 19:26:32 +0200

    Consistency with other setups.

--- Setup 02 - Webpack/webpack.config.js
+++ Setup 02 - Webpack/webpack.config.js
@@ -1,20 +1,40 @@
-/* global __dirname, module */
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2017 Rick Beerendonk   !*/
+
+/* eslint-disable */
+
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: "./site/src/app.jsx",
+  entry: ['./src/app.jsx'],
   output: {
-    path: __dirname + '/site/lib',
-    filename: "bundle.js"
+    path: path.join(__dirname, '/dist'),
+    filename: 'bundle.js'
   },
   devtool: 'source-map',
+  devServer: {
+    contentBase: './dist',
+    port: 9000
+  },
   mode: 'development',
   module: {
     rules: [
       {
-        test: /\.jsx$/,
-        exclude: /node_modules/,
-        loader: "babel-loader"
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules|packages/
       }
     ]
-  }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Setup Webpack & Babel'
+    }),
+    new webpack.NamedModulesPlugin()
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
 };
