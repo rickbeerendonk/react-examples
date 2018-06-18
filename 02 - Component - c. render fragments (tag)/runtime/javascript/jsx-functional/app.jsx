89e42720a011cd9f657a43d85557eae9f531523e
Rick Beerendonk <rick@beerendonk.com>
2018-06-18 15:50:58 +0200

    Fix Render Fragments in Functional examples.

--- 02 - Component - c. render fragments (tag)/runtime/javascript/jsx-functional/app.jsx
+++ 02 - Component - c. render fragments (tag)/runtime/javascript/jsx-functional/app.jsx
@@ -5,7 +5,7 @@
 
 const Greeting = () => <React.Fragment>
   <h1>Hello World!</h1>
-  'What do you think about React?'
+  What do you think about React?
   <div>It looks amazing.</div>
 </React.Fragment>;
 
--- 02 - Component - c. render fragments (tag)/systemjs/javascript/jsx-functional/Greeting.jsx
+++ 02 - Component - c. render fragments (tag)/systemjs/javascript/jsx-functional/Greeting.jsx
@@ -3,10 +3,10 @@
 
 import React from 'react';
 
-const Greeting = () => [
-  <h1 key="heading">Hello World!</h1>,
-  'What do you think about React?',
-  <div key="enthusiasm">It looks amazing.</div>
-];
+const Greeting = () => <React.Fragment>
+  <h1>Hello World!</h1>
+  What do you think about React?
+  <div>It looks amazing.</div>
+</React.Fragment>;
 
 export default Greeting;
