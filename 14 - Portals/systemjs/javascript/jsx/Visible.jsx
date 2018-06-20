23729c33dd1e01a5c94124b7e7258408b3d2a0a8
Rick Beerendonk <rick@beerendonk.com>
2018-06-20 12:14:56 +0200

    Add semicolon.

--- 14 - Portals/systemjs/javascript/jsx/Visible.jsx
+++ 14 - Portals/systemjs/javascript/jsx/Visible.jsx
@@ -7,6 +7,6 @@ import React from 'react';
 
 export default class Visible extends React.Component {
   render() {
-    return <h3>I am a visible portal, shown outside the react root by an invisible component!</h3>
+    return <h3>I am a visible portal, shown outside the react root by an invisible component!</h3>;
   }
 }
