d5c40cb84225ec47cc24e34db974a920ef27d547
Rick Beerendonk <rick@beerendonk.com>
2018-07-11 17:21:53 -0500

    Add HTML to JSX demo.

---
+++ JSX 02 - HTML to JSX/jsx/app.jsx
@@ -0,0 +1,71 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2017 Rick Beerendonk   !*/
+
+/* global HTMLtoJSX, React, ReactDOM */
+/* eslint react/prop-types:"off" */
+
+function jsxTransform(source) {
+  const converter = new HTMLtoJSX({
+    createClass: false,
+  });
+  return converter.convert(source);
+}
+
+function jsxTransformSafe(source) {
+  try {
+    const code = jsxTransform(source);
+    return { code, error: null };
+  }
+  catch (error) {
+    return { code: null, error };
+  }
+}
+
+class JsxCompiler extends React.Component {
+  constructor(props) {
+    super(props);
+  
+    this.html = `<!-- Hello world -->
+<div class="awesome" style="border: 1px solid red">
+  <label for="name">Enter your name: </label>
+  <input type="text" id="name" />
+</div>
+<p>Enter your HTML here</p>`;
+
+    this.state = jsxTransformSafe(this.html);
+
+    // Bind all non-react methods to this.
+    this.onChange = this.onChange.bind(this);
+  }
+  onChange(e) {
+    this.setState(jsxTransformSafe(e.target.value));
+  }
+  render() {
+    return (
+      <div style={styles.div}>
+        <textarea className="code" onChange={this.onChange} defaultValue={this.html} />
+        {
+          this.state.error
+          ? <textarea className="code" readOnly="readOnly" style={styles.error} value={this.state.error} />
+          : <textarea className="code" readOnly="readOnly" value={this.state.code} />
+        }
+      </div>);
+  }
+}
+
+var styles = {
+  div: {
+    display: 'grid',
+    gridTemplateColumns: '1fr 1fr',
+    gridTemplateRows: '45vh',
+    gridGap: '0.5vw'
+  },
+  error: {
+    color: 'red'
+  }
+};
+
+ReactDOM.render(
+  <JsxCompiler />,
+  document.getElementById('app')
+);
