5287f3981a290b424690d467efd112020f34210b
Rick Beerendonk <rick@beerendonk.com>
2018-07-25 15:56:07 +0200

    Add "State - async and event pool problem / solution" examples. 2 placeholders for alternative solutions.

---
+++ 05 - State - c1. async and event pool problem (onKeyUp)/systemjs/javascript/stage3/jsx/App.jsx
@@ -0,0 +1,24 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+/* eslint react/prop-types:"off" */
+
+import React from 'react';
+
+export default class App extends React.Component {
+  // Proposal: https://github.com/tc39/proposal-class-fields
+  // Support: http://kangax.github.io/compat-table/esnext/#test-class_fields
+  state = { pressed: '' };
+  onKeyUp = e => {
+    this.setState(prevState => ({ pressed: prevState.pressed + e.key }));
+  };
+
+  render() {
+    return (
+      <React.Fragment>
+        <input onKeyUp={this.onKeyUp} />
+        <div>Pressed: {this.state.pressed}</div>
+      </React.Fragment>
+    );
+  }
+}
