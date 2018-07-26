db2262fa7eee1ad013d9ee8a7c64ae2d9ee61570
Rick Beerendonk <rick@beerendonk.com>
2018-07-26 22:30:49 +0200

    Add "Component - render fragments (tag) - short syntax" compiled TypeScript example.

---
+++ 02 - Component - c2. render fragments (tag) - short syntax/compiled/typescript/site/src/index.tsx
@@ -0,0 +1,19 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2016 Rick Beerendonk   !*/
+
+//import * as React from 'react';
+//import * as ReactDOM from 'react-dom';
+
+class Greeting extends React.Component<undefined, undefined> {
+  render() {
+    return (
+      <>
+        <h1>Hello World!</h1>
+        What do you think about React?
+        <div>It looks amazing.</div>
+      </>
+    );
+  }
+}
+
+ReactDOM.render(<Greeting />, document.getElementById('app'));
