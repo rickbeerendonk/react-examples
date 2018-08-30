429b9db9424bd0a39364c177bff667e0f6e94776
Rick Beerendonk <rick@beerendonk.com>
2018-08-30 12:52:07 +0200

    Add SystemJS example.

---
+++ Styled-Components - 01 - Style element/systemjs/javascript/jsx/styled.jsx
@@ -0,0 +1,13 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+import styled from 'styled-components';
+
+export const StyledH1 = styled.h1`
+  text-align: center;
+  color: blue;
+`;
+
+export default {
+  StyledH1
+};
