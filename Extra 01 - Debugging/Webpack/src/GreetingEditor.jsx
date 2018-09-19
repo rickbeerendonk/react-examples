24315a17d3892d3ac3d651454af91990d11cfc52
Rick Beerendonk <rick@beerendonk.com>
2018-09-19 17:35:25 +0200

    Use React.Fragment instead of div.

--- Extra 01 - Debugging/Webpack/src/GreetingEditor.jsx
+++ Extra 01 - Debugging/Webpack/src/GreetingEditor.jsx
@@ -20,10 +20,10 @@ export default class GreetingEditor extends React.Component {
   }
   render() {
     return (
-      <div>
+      <React.Fragment>
         <EditBox onChange={this.onChange} name={this.state.name} />
         <Greeting name={this.state.name} />
-      </div>
+      </React.Fragment>
     );
   }
 }
