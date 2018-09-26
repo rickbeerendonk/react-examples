d04362e8bbb018821c9950624aa3bfee5102b8f6
Rick Beerendonk <rick@beerendonk.com>
2018-09-26 11:58:11 +0200

    Place inline vs method in the example side by side.

--- 07 - Multiple Components/systemjs/javascript/jsx/EditBox.jsx
+++ 07 - Multiple Components/systemjs/javascript/jsx/EditBox.jsx
@@ -5,8 +5,30 @@
 
 import React from 'react';
 
+/* Inline function */
+
 const EditBox = ({ onChange, value }) => (
   <input onChange={e => onChange(e.target.value)} value={value} />
 );
+
+/* method */
+
+/*
+class EditBox extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.onChange = this.onChange.bind(this);
+  }
+  onChange(e) {
+    this.props.onChange(e.target.value);
+  }
+  render() {
+    return (
+      <input onChange={this.onChange} value={this.props.value} />
+    );
+  }
+}
+*/
 
 export default EditBox;
