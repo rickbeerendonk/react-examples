fe3703952dfcc7ef9529ef851c5150098c0768e9
Rick Beerendonk <rick@beerendonk.com>
2018-09-26 06:40:19 +0200

    Add Profiler API example.

---
+++ 24 - Profiler API/systemjs/javascript/jsx/GreetingEditor.jsx
@@ -0,0 +1,49 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+/* eslint react/prop-types:"off" */
+/* eslint no-console:"off" */
+
+import React from 'react';
+
+import EditBox from './EditBox';
+import Greeting from './Greeting';
+
+export default class GreetingEditor extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: 'World' };
+
+    this.onChange = this.onChange.bind(this);
+  }
+  myProfilerRendered(id, phase, actualTime, baseTime, startTime, commitTime) {
+    // source: https://github.com/reactjs/rfcs/pull/51
+    console.group('Profiler');
+    console.log('ID: ' + id);
+    console.log('Phase (mount|update): ' + phase);
+    console.log('Actual Time: ' + actualTime);
+    console.log('Base Time: ' + baseTime);
+    console.log('Start Time: ' + startTime);
+    console.log('Commit Time: ' + commitTime);
+    console.groupEnd();
+  }
+  onChange(name) {
+    this.setState({ name });
+  }
+  render() {
+    return (
+      <React.Fragment>
+        {/* Component(s) not measured... */}
+        <EditBox onChange={this.onChange} value={this.state.name} />
+
+        {/* Component(s) measured... */}
+        <React.unstable_Profiler
+          id="myProfiler"
+          onRender={this.myProfilerRendered}
+        >
+          <Greeting name={this.state.name} />
+        </React.unstable_Profiler>
+      </React.Fragment>
+    );
+  }
+}
