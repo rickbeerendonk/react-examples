2acc790ea3847a2a4ab0fd34aae0fd004c6d5b60
Rick Beerendonk <rick@beerendonk.com>
2018-11-03 09:37:42 +0100

    Add "Hooks - useEffect - Force sync update" example (not supported yet).

---
+++ 05 - Hooks - b. useEffect - b3. [no support yet] force sync updates (onClick)/systemjs/javascript/jsx/ClickMe.jsx
@@ -0,0 +1,27 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+/* eslint react/prop-types:"off" */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+// SystemJS limitation (we should be able to do this directly in import)
+const { useState } = React;
+
+function ClickMe() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <a
+      onClick={() => {
+        ReactDOM.flushSync(() => setCount(count + 0.4));
+        ReactDOM.flushSync(() => setCount(count + 0.6));
+      }}
+    >
+      {`This link has been clicked ${count} times`}
+    </a>
+  );
+}
+
+export default ClickMe;
