ce41bb85e88108f2ee21faeb351e77ad843d49f4
Rick Beerendonk <rick@beerendonk.com>
2018-12-11 20:08:22 +0100

    Add function examples with Hooks.

---
+++ 05 - State - d2. batched updates and event pool solution - event persist (onKeyUp)/runtime/js/function/index.jsx
@@ -0,0 +1,22 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+/* global React, ReactDOM */
+
+function App() {
+  const [pressed, setPressed] = React.useState('');
+
+  function handleKeyUp(e) {
+    e.persist();
+    setPressed(pressed => pressed + e.key);
+  }
+
+  return (
+    <React.Fragment>
+      <input onKeyUp={handleKeyUp} />
+      <div>Pressed: {pressed}</div>
+    </React.Fragment>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ 05 - State - d2. batched updates and event pool solution - event persist (onKeyUp)/systemjs/js/function/App.jsx
@@ -0,0 +1,22 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+import React from 'react';
+
+function App() {
+  const [pressed, setPressed] = React.useState('');
+
+  function handleKeyUp(e) {
+    e.persist();
+    setPressed(pressed => pressed + e.key);
+  }
+
+  return (
+    <React.Fragment>
+      <input autoFocus onKeyUp={handleKeyUp} />
+      <div>Pressed: {pressed}</div>
+    </React.Fragment>
+  );
+}
+
+export default App;
