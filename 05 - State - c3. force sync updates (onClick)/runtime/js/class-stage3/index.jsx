f38a1d3b72b00e3a315ab3a495084d5019f9f3f4
Rick Beerendonk <rick@beerendonk.com>
2018-12-12 10:17:04 +0100

    Add "State - Force sync updates" runtime examples.

---
+++ 05 - State - c3. force sync updates (onClick)/runtime/js/class-stage3/index.jsx
@@ -0,0 +1,30 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2014 Rick Beerendonk   !*/
+
+/* global React, ReactDOM */
+
+class ClickMe extends React.Component {
+  // Proposal: https://github.com/tc39/proposal-class-fields
+  // Support: http://kangax.github.io/compat-table/esnext/#test-class_fields
+  state = { count: 0 };
+  handleClick = () => {
+    // Updates are batched for performance reasons.
+    // Use ReactDOM.flushSync():
+    ReactDOM.flushSync(() => {
+      this.setState({ count: this.state.count + 0.4 });
+    });
+    ReactDOM.flushSync(() => {
+      this.setState({ count: this.state.count + 0.6 });
+    });
+  };
+
+  render() {
+    return (
+      <a onClick={this.handleClick}>
+        {`This link has been clicked ${this.state.count} times`}
+      </a>
+    );
+  }
+}
+
+ReactDOM.render(<ClickMe />, document.getElementById('app'));
---
+++ 05 - State - c3. force sync updates (onClick)/runtime/js/no-jsx/index.js
@@ -0,0 +1,35 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2016 Rick Beerendonk   !*/
+
+/* global React, ReactDOM */
+
+class ClickMe extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+
+    // Bind all non-react methods to this.
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    // Updates are batched for performance reasons.
+    // Use ReactDOM.flushSync():
+    ReactDOM.flushSync(() => {
+      this.setState({ count: this.state.count + 0.4 });
+    });
+    ReactDOM.flushSync(() => {
+      this.setState({ count: this.state.count + 0.6 });
+    });
+  }
+  render() {
+    return React.createElement(
+      'a',
+      { onClick: this.handleClick },
+      'This link has been clicked ',
+      this.state.count,
+      ' times'
+    );
+  }
+}
+
+ReactDOM.render(React.createElement(ClickMe), document.getElementById('app'));
