90d1dfe409eca1ffdefcbd3e19f9ed89bf63bee7
Rick Beerendonk <rick@beerendonk.com>
2019-01-03 00:58:58 +0100

    One component per file.

---
+++ Router - 01 - Simple/compiled/js/src/Home.jsx
@@ -0,0 +1,10 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+import React from 'react';
+
+function Home() {
+  return <h2>Home</h2>;
+}
+
+export default Home;
