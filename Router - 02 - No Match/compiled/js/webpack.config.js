fc42909f3f4e44b621e1a6f7d3a5b68266ee00d6
Rick Beerendonk <rick@beerendonk.com>
2019-01-04 23:33:16 +0100

    Correct Title.

--- Router - 02 - No Match/compiled/js/webpack.config.js
+++ Router - 02 - No Match/compiled/js/webpack.config.js
@@ -34,7 +34,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Router - Simple'
+      title: 'Router - No Match'
     }),
     new webpack.NamedModulesPlugin()
   ],
