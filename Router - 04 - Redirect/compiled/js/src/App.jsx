c3894ec65e7a210341c58773f2797427421fbaec
Rick Beerendonk <rick@beerendonk.com>
2019-01-06 14:19:40 +0100

    Add "Router - Redirect" examples.

---
+++ Router - 04 - Redirect/compiled/js/src/App.jsx
@@ -0,0 +1,49 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2018 Rick Beerendonk   !*/
+
+import React from 'react';
+import { BrowserRouter, Route, Redirect, Switch, Link } from 'react-router-dom';
+
+import Home from './Home';
+import Page1 from './Page1';
+import Page2 from './Page2';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <>
+        <header>
+          <h1>App</h1>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/page1">Page 1</Link>
+            </li>
+            <li>
+              <Link to="/page2">Page 2</Link>
+            </li>
+            <li>
+              <Link to="/oldpage2">Old Page 2</Link>
+            </li>
+          </ul>
+        </header>
+
+        <main>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/page1" component={Page1} />
+            <Route path="/page2" component={Page2} />
+            {/* Redirect from can only be used inside Switch */}
+            <Redirect from="/oldpage2" to="/page2" />
+          </Switch>
+        </main>
+
+        <footer>My copyright</footer>
+      </>
+    </BrowserRouter>
+  );
+}
+
+export default App;
