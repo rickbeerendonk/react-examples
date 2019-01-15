1e2c4e51e3ad5333490bf5152737b8a768014efb
Rick Beerendonk <rick@beerendonk.com>
2019-01-15 20:21:20 +0100

    Correct titles.

--- Setup 03 - ESLint/webpack.config.js
+++ Setup 03 - ESLint/webpack.config.js
@@ -40,7 +40,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Setup Webpack, ESLint & Babel'
+      title: 'Setup ESLint, Webpack & Babel'
     }),
     new webpack.NamedModulesPlugin()
   ],
