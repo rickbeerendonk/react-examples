59a5e165c83cc0d21be7dab11e50e18d315c7fcc
Rick Beerendonk <rick@beerendonk.com>
2019-01-16 01:10:44 +0100

    Add "Test - Jest - Setup" example.

---
+++ Test 02 - Jest - a. Setup/src/Greeting.jsx
@@ -0,0 +1,12 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2017 Rick Beerendonk   !*/
+
+import React from 'react';
+
+import './Greeting.css';
+
+function Greeting() {
+  return <h1 className="greeting">Hello World!</h1>;
+}
+
+export default Greeting;
---
+++ Test 02 - Jest - a. Setup/webpack.config.js
@@ -0,0 +1,50 @@
+/*! Mozilla Public License Version 2.0 !*/
+/*! Copyright © 2017 Rick Beerendonk   !*/
+
+/* eslint-disable */
+
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: ['./src/app.jsx'],
+  output: {
+    path: path.join(__dirname, '/build'),
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './build',
+    port: 9100
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        enforce: 'pre',
+        test: /\.jsx?$/,
+        use: 'eslint-loader',
+        exclude: /node_modules|packages/
+      },
+      {
+        test: /\.jsx?$/,
+        use: 'babel-loader',
+        exclude: /node_modules|packages/
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Setup Webpack, ESLint & Babel'
+    }),
+    new webpack.NamedModulesPlugin()
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  }
+};
