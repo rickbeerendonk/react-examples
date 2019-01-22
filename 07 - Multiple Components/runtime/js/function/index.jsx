03c7bda022b2ba73efb17b51b930ce99f5473610
Rick Beerendonk <rick@beerendonk.com>
2019-01-22 22:39:57 +0100

    Add runtime version of function example.

---
+++ 07 - Multiple Components/runtime/js/function/index.jsx
@@ -0,0 +1,27 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2014 Rick Beerendonk          !*/
+
+/* global React, ReactDOM */
+
+function EditBox({ onChange, value }) {
+  return (
+    <input autoFocus onChange={e => onChange(e.target.value)} value={value} />
+  );
+}
+
+function Greeting({ name }) {
+  return <h1>Hello {name}!</h1>;
+}
+
+function GreetingEditor() {
+  const [name, setName] = React.useState('World');
+
+  return (
+    <React.Fragment>
+      <EditBox onChange={setName} value={name} />
+      <Greeting name={name} />
+    </React.Fragment>
+  );
+}
+
+ReactDOM.render(<GreetingEditor />, document.getElementById('app'));
