4d03820cb5728612ee085b73fb21e86c65f894d1
Rick Beerendonk <rick@beerendonk.com>
2019-01-22 23:02:30 +0100

    Add SystemJS js class example.

---
+++ 12 - Suspense - c. Lazy/systemjs/js/class/App.jsx
@@ -0,0 +1,33 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import React from 'react';
+const Greeting = React.lazy(() => import('./Greeting'));
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { visible: false };
+
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+  }
+  handleButtonClick() {
+    this.setState(state => ({ visible: !state.visible }));
+  }
+  render() {
+    return (
+      <React.Fragment>
+        <button onClick={this.handleButtonClick}>
+          {this.state.visible ? 'Hide' : 'Show'}
+        </button>
+        {this.state.visible && (
+          <React.Suspense fallback={<div>Loading...</div>}>
+            <Greeting />
+          </React.Suspense>
+        )}
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
