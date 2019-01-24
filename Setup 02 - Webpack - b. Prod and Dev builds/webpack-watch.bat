658d57b1c4febc3774372f1215a877fdd0812d8f
Rick Beerendonk <rick@beerendonk.com>
2019-01-24 13:55:37 +0100

    Simplification.

--- Setup 02 - Webpack - b. Prod and Dev builds/webpack-watch.bat
+++
@@ -1,5 +0,0 @@
-
-REM First:
-REM npm install
-
-.\node_modules\.bin\webpack -d --progress --watch
--- Setup 02 - Webpack - b. Prod and Dev builds/webpack-watch.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# First:
-# npm install
-
-./node_modules/.bin/webpack -d --progress --watch
