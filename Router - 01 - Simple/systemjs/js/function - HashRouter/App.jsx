4954daf8b1cc0dbee38fa6806c069c8765663d80
Rick Beerendonk <rick@beerendonk.com>
2019-01-25 11:55:09 +0100

    Add HashRouter example.

---
+++ Router - 01 - Simple/systemjs/js/function - HashRouter/App.jsx
@@ -0,0 +1,42 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import React from 'react';
+import { HashRouter, Route, Link } from 'react-router-dom';
+
+import Home from './Home';
+import Page1 from './Page1';
+import Page2 from './Page2';
+
+function App() {
+  return (
+    <HashRouter>
+      <React.Fragment>
+        <header>
+          <h1>App</h1>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/page1">Page 1</Link>
+            </li>
+            <li>
+              <Link to="/page2">Page 2</Link>
+            </li>
+          </ul>
+        </header>
+
+        <main>
+          <Route exact path="/" component={Home} />
+          <Route path="/page1" component={Page1} />
+          <Route path="/page2" render={props => <Page2 {...props} />} />
+        </main>
+
+        <footer>My copyright</footer>
+      </React.Fragment>
+    </HashRouter>
+  );
+}
+
+export default App;
