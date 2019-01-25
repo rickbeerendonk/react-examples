08a9484fc725f32b705335ecf8a79ebfed9ade6b
Rick Beerendonk <rick@beerendonk.com>
2019-01-25 11:57:09 +0100

    Add HashRouter example.

---
+++ Router - 03 - Redirect/systemjs/js/function - HashRouter/Page2.jsx
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import React from 'react';
+
+function Page2() {
+  return <h2>2nd page</h2>;
+}
+
+export default Page2;
