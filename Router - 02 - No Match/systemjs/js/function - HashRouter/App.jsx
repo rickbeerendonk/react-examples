da62f13e79118777eb39a79d6ef2164960fad470
Rick Beerendonk <rick@beerendonk.com>
2019-01-25 11:56:08 +0100

    Add HashRouter example.

---
+++ Router - 02 - No Match/systemjs/js/function - HashRouter/App.jsx
@@ -0,0 +1,51 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import React from 'react';
+import { HashRouter, Route, Switch, Link } from 'react-router-dom';
+
+import Home from './Home';
+import NoMatch from './NoMatch';
+import Page1 from './Page1';
+import Page2 from './Page2';
+
+function App() {
+  return (
+    <HashRouter>
+      <React.Fragment>
+        <header>
+          <h1>App</h1>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/page1">Page 1</Link>
+            </li>
+            <li>
+              <Link to="/page2">Page 2</Link>
+            </li>
+            <li>
+              <Link to="/page404">Page 404</Link>
+            </li>
+          </ul>
+        </header>
+
+        <main>
+          {/* A <Switch> renders the first child <Route> that matches. 
+            A <Route> with no path always matches. */}
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/page1" component={Page1} />
+            <Route path="/page2" component={Page2} />
+            <Route component={NoMatch} />
+          </Switch>
+        </main>
+
+        <footer>My copyright</footer>
+      </React.Fragment>
+    </HashRouter>
+  );
+}
+
+export default App;
---
+++ Router - 02 - No Match/systemjs/js/function - HashRouter/NoMatch.jsx
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import React from 'react';
+
+function NoMatch() {
+  return <h2>No match!</h2>;
+}
+
+export default NoMatch;
