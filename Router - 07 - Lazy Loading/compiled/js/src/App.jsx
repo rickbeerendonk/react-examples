44134fb47bf20386c6f08b36f3e721af402b5b43
Rick Beerendonk <rick@beerendonk.com>
2019-01-30 11:32:15 +0100

    Use nav-tag.

--- Router - 07 - Lazy Loading/compiled/js/src/App.jsx
+++ Router - 07 - Lazy Loading/compiled/js/src/App.jsx
@@ -14,17 +14,11 @@ function App() {
       <>
         <header>
           <h1>App</h1>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/page1">Page 1</Link>
-            </li>
-            <li>
-              <Link to="/page2">Page 2</Link>
-            </li>
-          </ul>
+          <nav>
+            <Link to="/">Home</Link>
+            <Link to="/page1">Page 1</Link>
+            <Link to="/page2">Page 2</Link>
+          </nav>
         </header>
 
         <main>
