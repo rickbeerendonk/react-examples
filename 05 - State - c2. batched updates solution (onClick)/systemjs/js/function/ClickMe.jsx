0396e0dc18e35c453986dbffb4143c9ba6c6fd11
Rick Beerendonk <rick@beerendonk.com>
2019-03-05 15:22:51 +0100

    No inline handler (just like in TypeScript example).

--- 05 - State - c2. batched updates solution (onClick)/systemjs/js/function/ClickMe.jsx
+++ 05 - State - c2. batched updates solution (onClick)/systemjs/js/function/ClickMe.jsx
@@ -6,16 +6,15 @@ import React from 'react';
 function ClickMe() {
   const [count, setCount] = React.useState(0);
 
-  return (
-    <a
-      onClick={() => {
-        setCount(c => c + 0.4);
-        setCount(c => c + 0.6);
-      }}
-    >
-      This link has been clicked {count} times
-    </a>
-  );
+  function handleClick() {
+    // Updates are batched for performance reasons.
+    // Therefore a callback should be used:
+
+    setCount(c => c + 0.4);
+    setCount(c => c + 0.6);
+  }
+
+  return <a onClick={handleClick}>This link has been clicked {count} times</a>;
 }
 
 export default ClickMe;
