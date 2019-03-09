4e33934e7603cd340ee61db3d783913a6db5ecc2
Rick Beerendonk <rick@beerendonk.com>
2019-03-09 22:30:39 +0100

    Add "Suspense - Lazy - pre-load" examples.

---
+++ 12 - Suspense - c. Lazy - i. pre-load/systemjs/js/class/Greeting.jsx
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import React from 'react';
+
+function Greeting() {
+  return <h1 className="greeting">Hello World!</h1>;
+}
+
+export default Greeting;
