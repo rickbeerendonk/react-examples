45cce4083a4b2bf0a03b0c8c4f8071615de190db
Rick Beerendonk <rick@beerendonk.com>
2019-03-11 09:29:04 +0100

    Don't change handlers on every render.

--- 14 - Hooks - a. Custom - ii. Document Hidden/systemjs/js/function/hooks/hidden.js
+++ 14 - Hooks - a. Custom - ii. Document Hidden/systemjs/js/function/hooks/hidden.js
@@ -7,16 +7,17 @@ function useIsHidden() {
   // Hidden state variable
   const [isHidden, setIsHidden] = React.useState(document.hidden);
 
-  // Function to change state
-  function handleVisibilityChange() {
-    setIsHidden(document.hidden);
-  }
   React.useEffect(() => {
+    // Function to change state
+    function handleVisibilityChange() {
+      setIsHidden(document.hidden);
+    }
+
     document.addEventListener('visibilitychange', handleVisibilityChange);
     return () => {
       document.removeEventListener('visibilitychange', handleVisibilityChange);
     };
-  }, [handleVisibilityChange]);
+  }, []);
 
   // Return current state
   return isHidden;
