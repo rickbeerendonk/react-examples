6478f5dea8c05437fb20ceb68eec9487dadab9eb
Rick Beerendonk <rick@beerendonk.com>
2019-03-13 17:57:39 +0100

    Add more error cases for Exception Bounderies.

--- 09 - Exceptions - a. Error Boundaries/systemjs/js/function/GreetingEditor.jsx
+++ 09 - Exceptions - a. Error Boundaries/systemjs/js/function/GreetingEditor.jsx
@@ -6,9 +6,27 @@ import React from 'react';
 function GreetingEditor() {
   const [name, setName] = React.useState('World');
   React.useLayoutEffect(() => {
-    if (name === 'Error') {
-      throw new Error('Error');
+    if (name === 'layoutEffectError') {
+      throw new Error('layoutEffectError');
     }
+
+    return () => {
+      if (name === 'cleanLayoutEffectError') {
+        throw new Error('cleanLayoutEffectError');
+      }
+    };
+  });
+
+  React.useEffect(() => {
+    if (name === 'effectError') {
+      throw new Error('effectError');
+    }
+
+    return () => {
+      if (name === 'cleanEffectError') {
+        throw new Error('cleanEffectError');
+      }
+    };
   });
 
   function handleChange(e) {
@@ -23,6 +41,13 @@ function GreetingEditor() {
     <React.Fragment>
       <input autoFocus onChange={handleChange} value={name} />
       <h1>Hello {name}!</h1>
+      <ul style={{ color: 'darkgrey' }}>
+        <li>layoutEffectError</li>
+        <li>cleanLayoutEffectError [during next render]</li>
+        <li>effectError</li>
+        <li>cleanEffectError [during next render]</li>
+        <li>renderException</li>
+      </ul>
     </React.Fragment>
   );
 }
