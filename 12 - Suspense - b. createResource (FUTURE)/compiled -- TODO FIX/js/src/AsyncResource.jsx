a3d1942a568ff1652ce0ca66052bed2de8c37899
Rick Beerendonk <rick@beerendonk.com>
2019-03-16 20:37:39 +0100

    Fix outdated cache.

--- 12 - Suspense - b. createResource (FUTURE)/compiled -- TODO FIX/js/src/AsyncResource.jsx
+++ 12 - Suspense - b. createResource (FUTURE)/compiled -- TODO FIX/js/src/AsyncResource.jsx
@@ -1,9 +1,9 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2018 Rick Beerendonk          !*/
 
-import { unstable_createResource as createResource } from 'react-cache';
+import * as ReactCache from 'react-cache';
 
-const AsyncResource = createResource(
+const AsyncResource = ReactCache.unstable_createResource(
   id =>
     new Promise(resolve => {
       setTimeout(() => resolve(`Result ${id}!`), 3000);
