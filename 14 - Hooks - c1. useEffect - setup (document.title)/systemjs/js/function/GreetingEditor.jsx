c24259303cf9cc38044fe5a009a85913f256bd96
Rick Beerendonk <rick@beerendonk.com>
2019-03-19 15:28:47 +0100

    Improve example.

--- 14 - Hooks - c1. useEffect - setup (document.title)/systemjs/js/function/GreetingEditor.jsx
+++ 14 - Hooks - c1. useEffect - setup (document.title)/systemjs/js/function/GreetingEditor.jsx
@@ -3,25 +3,35 @@
 
 import React from 'react';
 
+/* eslint no-console: 0 */
+
 function GreetingEditor() {
   const [name, setName] = React.useState('World');
+  const [other, setOther] = React.useState('Other');
 
   // 2nd parameter:
   // - None: Always updates
   // - []: Only updates first time (wrong)
   // - [name]: Only updates if name changes
   React.useEffect(() => {
+    console.log('effect');
     document.title = name;
-  });
+  }, [name]);
 
-  function handleChange(e) {
+  function handleChange1(e) {
     setName(e.target.value);
+  }
+
+  function handleChange2(e) {
+    setOther(e.target.value);
   }
 
   return (
     <React.Fragment>
-      <input autoFocus onChange={handleChange} value={name} />
+      <input autoFocus onChange={handleChange1} value={name} />
       <h1>Hello {name}!</h1>
+      <input autoFocus onChange={handleChange2} value={other} />
+      <h2>Hello {other}!</h2>
     </React.Fragment>
   );
 }
