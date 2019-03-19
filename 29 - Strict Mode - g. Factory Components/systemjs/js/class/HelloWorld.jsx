5165a3799959c880507fd6954a094b2a2bc1bc26
Rick Beerendonk <rick@beerendonk.com>
2019-03-19 21:31:21 +0100

    Add "Strict Mode - Factory Components" example.

---
+++ 29 - Strict Mode - g. Factory Components/systemjs/js/class/HelloWorld.jsx
@@ -0,0 +1,17 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+/* eslint react/display-name:"off" */
+
+import React from 'react';
+
+function HelloWorld() {
+  return {
+    state: { value: 'World' },
+    render: function() {
+      return <h1>Hello {this.state.value}!</h1>;
+    }
+  };
+}
+
+export default HelloWorld;
---
+++ 29 - Strict Mode - g. Factory Components/systemjs/js/class/systemjs.config.js
@@ -0,0 +1,33 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+/* global SystemJS */
+
+SystemJS.config({
+  meta: {
+    '*.jsx': {
+      babelOptions: {
+        es2015: true,
+        react: true
+      }
+    }
+  },
+  paths: {
+    // paths serve as alias
+    'npm:': '../../../../node_modules/',
+    'npm-unstable:': '../../../../node_modules_unstable/'
+  },
+  map: {
+    'plugin-babel': 'npm:systemjs-plugin-babel/plugin-babel.js',
+    'systemjs-babel-build':
+      'npm:systemjs-plugin-babel/systemjs-babel-browser.js',
+    react: 'npm-unstable:react/umd/react.development.js',
+    'react-dom': 'npm-unstable:react-dom/umd/react-dom.development.js'
+  },
+  packages: {
+    '.': {
+      defaultExtension: 'jsx'
+    }
+  },
+  transpiler: 'plugin-babel'
+});
