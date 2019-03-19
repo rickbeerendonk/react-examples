aa90a8f7340ed39cbc464665009b2792facc576e
Rick Beerendonk <rick@beerendonk.com>
2019-03-19 21:01:15 +0100

    Add "Strict Mode - findDOMNode" example.

--- 29 - Strict Mode - e. findDOMNode/TODO.md
+++
@@ -1,3 +0,0 @@
-See: https://github.com/facebook/react/pull/13841
-
-Alternative Fragment Refs RFC: https://github.com/reactjs/rfcs/pull/97
---
+++ 29 - Strict Mode - e. findDOMNode/systemjs/js/class/index.jsx
@@ -0,0 +1,14 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import HelloWorld from './HelloWorld';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <HelloWorld />
+  </React.StrictMode>,
+  document.getElementById('app')
+);
