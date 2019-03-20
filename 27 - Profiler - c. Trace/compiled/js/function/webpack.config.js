8e4396e55d106dd5358a619d3119ba2455a27e55
Rick Beerendonk <rick@beerendonk.com>
2019-03-20 09:06:38 +0100

    Add compiled demo (that works).

---
+++ 27 - Profiler - c. Trace/compiled/js/function/webpack.config.js
@@ -0,0 +1,44 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2017 Rick Beerendonk          !*/
+
+/* eslint-disable */
+
+const path = require('path');
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/app.jsx',
+  output: {
+    path: path.join(__dirname, '/build'),
+    filename: 'bundle.js'
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './build',
+    port: 9100
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: 'babel-loader',
+        exclude: /node_modules|packages/
+      },
+      {
+        test: /\.css$/,
+        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'React Profiler Trace'
+    }),
+    new webpack.NamedModulesPlugin()
+  ],
+  resolve: {
+    extensions: ['.js', '.jsx', '.json']
+  }
+};
