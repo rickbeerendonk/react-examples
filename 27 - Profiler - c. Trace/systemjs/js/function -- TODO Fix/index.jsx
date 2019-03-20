f696d578b78997ccae5df2174f54f69ba6377c77
Rick Beerendonk <rick@beerendonk.com>
2019-03-20 05:43:46 +0100

    Add "Profiler - Trace" example.

---
+++ 27 - Profiler - c. Trace/systemjs/js/function -- TODO Fix/index.jsx
@@ -0,0 +1,9 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import GreetingEditor from './GreetingEditor';
+
+ReactDOM.render(<GreetingEditor />, document.getElementById('app'));
