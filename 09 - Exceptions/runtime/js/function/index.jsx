3925a69c36763d9d6f38155d70fce8e8617d8c9a
Rick Beerendonk <rick@beerendonk.com>
2019-03-22 12:44:54 +0100

    Add runtime example.

---
+++ 09 - Exceptions/runtime/js/function/index.jsx
@@ -0,0 +1,30 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+/* global React, ReactDOM */
+
+function GreetingEditor() {
+  const [name, setName] = React.useState('World');
+  React.useLayoutEffect(() => {
+    if (name === 'Error') {
+      throw new Error('Error');
+    }
+  });
+
+  function handleChange(e) {
+    setName(e.target.value);
+  }
+
+  if (name === 'renderException') {
+    throw new Error('render exception');
+  }
+
+  return (
+    <React.Fragment>
+      <input autoFocus onChange={handleChange} value={name} />
+      <h1>Hello {name}!</h1>
+    </React.Fragment>
+  );
+}
+
+ReactDOM.render(<GreetingEditor />, document.getElementById('app'));
