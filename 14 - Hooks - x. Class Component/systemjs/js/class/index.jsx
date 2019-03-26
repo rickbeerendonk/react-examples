1b8aafbb83ea791c7bd8ff993d6188958ff1d12e
Rick Beerendonk <rick@beerendonk.com>
2019-03-26 08:06:41 -0500

    Delete idea to turn into npm package.

--- 14 - Hooks - x. Class Component/systemjs/js/class/index.jsx
+++
@@ -1,9 +0,0 @@
-/*! European Union Public License version 1.2 !*/
-/*! Copyright © 2019 Rick Beerendonk          !*/
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './App';
-
-ReactDOM.render(<App prop1="prop1" />, document.getElementById('app'));
