198a171c2e0bf05332ae85625a76ea6f9a9e6e6c
Rick Beerendonk <rick@beerendonk.com>
2019-04-10 10:42:00 -0500

    Add React.jsx() "Component - Render one element" example.

---
+++ 02 - Component - b. Render one element/systemjs/js/no-jsx-future/Greeting.js
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import React from 'react';
+
+function Greeting() {
+  return React.jsx('h1', { children: 'Hello World!' });
+}
+
+export default Greeting;
