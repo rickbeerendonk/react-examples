a6789f95b4186bbfec5469029b385e8fb8d088de
Rick Beerendonk <rick@beerendonk.com>
2019-04-15 17:23:23 -0500

    Rename and correct copyright.

--- 03 - Properties/systemjs/js/future-jsx()/index.js
+++ 03 - Properties/systemjs/js/future-jsx()/index.js
@@ -1,5 +1,5 @@
 /*! European Union Public License version 1.2 !*/
-/*! Copyright © 2018 Rick Beerendonk          !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
 
 import React from 'react';
 import ReactDOM from 'react-dom';
