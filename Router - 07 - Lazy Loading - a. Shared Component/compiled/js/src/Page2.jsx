5041b3d786d974bc3792b3e839f170e368a74192
Rick Beerendonk <rick@beerendonk.com>
2019-05-09 23:33:40 +0200

    Add "Router - Lazy Loading - Shared Component" example.

---
+++ Router - 07 - Lazy Loading - a. Shared Component/compiled/js/src/Page2.jsx
@@ -0,0 +1,17 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import React from 'react';
+
+import Shared from './Shared';
+
+function Page2() {
+  return (
+    <>
+      <h2>2nd page</h2>
+      <Shared />
+    </>
+  );
+}
+
+export default Page2;
---
+++ Router - 07 - Lazy Loading - a. Shared Component/compiled/js/src/Shared.jsx
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import React from 'react';
+
+function Shared() {
+  return <h3>Shared</h3>;
+}
+
+export default Shared;
