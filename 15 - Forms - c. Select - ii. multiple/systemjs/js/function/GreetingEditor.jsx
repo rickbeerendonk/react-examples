2b76556878989b2a7c69c06342ca157bc117fa2b
Rick Beerendonk <rick@beerendonk.com>
2019-05-23 13:55:46 +0200

    Add "Forms - Select - multiple" function example.

---
+++ 15 - Forms - c. Select - ii. multiple/systemjs/js/function/GreetingEditor.jsx
@@ -0,0 +1,24 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import React from 'react';
+
+function GreetingEditor() {
+  const [selected, setSelected] = React.useState(['bike', 'train']);
+
+  function handleChange(e) {
+    setSelected(
+      [...e.target.options].filter(o => o.selected).map(o => o.value)
+    );
+  }
+
+  return (
+    <select multiple onChange={handleChange} value={selected}>
+      <option value="bike">Bike</option>
+      <option value="car">Car</option>
+      <option value="train">Train</option>
+    </select>
+  );
+}
+
+export default GreetingEditor;
