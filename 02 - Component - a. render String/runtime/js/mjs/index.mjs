895d3bc6649d908792465bf036fdfb0094ae2950
Rick Beerendonk <rick@beerendonk.com>
2019-08-30 09:42:58 +0200

    Add module example.

---
+++ 02 - Component - a. render String/runtime/js/mjs/index.mjs
@@ -0,0 +1,9 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2016 Rick Beerendonk          !*/
+
+import React from './react.development.mjs';
+import ReactDOM from './react-dom.development.mjs';
+
+import Greeting from './greeting.mjs';
+
+ReactDOM.render(React.createElement(Greeting), document.getElementById('app'));
