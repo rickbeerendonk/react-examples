024f1baca104430b924bb3a999963157338fce23
Rick Beerendonk <rick@beerendonk.com>
2019-09-06 08:52:24 +0200

    Add build for unstable React (with issues).

---
+++ Setup 02 - Webpack - c. React Unstable/src/Greeting.jsx
@@ -0,0 +1,12 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2017 Rick Beerendonk          !*/
+
+import React from '../../node_modules_unstable/react/';
+
+import './Greeting.css';
+
+function Greeting() {
+  return <h1 className="greeting">Hello World!</h1>;
+}
+
+export default Greeting;
