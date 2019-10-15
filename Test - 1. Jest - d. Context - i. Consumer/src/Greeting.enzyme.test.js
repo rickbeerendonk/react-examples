bd3de2ad538f35c7d1818c96751b944c2e609af5
Rick Beerendonk <rick@beerendonk.com>
2019-10-15 04:29:05 +0200

    Add non-snapshot version.

--- Test - 1. Jest - d. Context - i. Consumer/src/Greeting.enzyme.test.js
+++ Test - 1. Jest - d. Context - i. Consumer/src/Greeting.enzyme.test.js
@@ -7,6 +7,11 @@ import Greeting from './Greeting';
 configure({ adapter: new Adapter() });
 
 /// Enzyme ///
+it('renders hello world with default context color (enzyme) (enzyme)', () => {
+  const wrapper = render(<Greeting name="World" />);
+  expect(wrapper.prop('style').color).toEqual('#d676ff');
+});
+
 it('renders hello world snapshot with default context color (enzyme)', () => {
   const wrapper = render(<Greeting name="World" />);
   expect(toJson(wrapper)).toMatchSnapshot();
