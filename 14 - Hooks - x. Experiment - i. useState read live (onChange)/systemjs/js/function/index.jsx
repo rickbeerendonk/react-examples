08f2df83736e4fe599f2cbd6bf120e39a648e359
Rick Beerendonk <rick@beerendonk.com>
2019-10-26 09:24:12 -0500

    Remove experiment (experiments are in separate project).

--- 14 - Hooks - x. Experiment - i. useState read live (onChange)/systemjs/js/function/index.jsx
+++
@@ -1,9 +0,0 @@
-/*! European Union Public License version 1.2 !*/
-/*! Copyright © 2019 Rick Beerendonk          !*/
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import GreetingEditor from './GreetingEditor';
-
-ReactDOM.render(<GreetingEditor />, document.getElementById('app'));
