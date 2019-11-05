054c0e88079110f2a0ec96bdf05edf65ce7ca986
Rick Beerendonk <rick@beerendonk.com>
2019-11-05 08:42:57 -0500

    All "Render Component" examples in one folder.

---
+++ 02 - Render Component/a. String/systemjs/ts/class/Greeting.tsx
@@ -0,0 +1,12 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import React from 'react';
+
+class Greeting extends React.Component {
+  render(): string {
+    return 'Hello World!';
+  }
+}
+
+export default Greeting;
