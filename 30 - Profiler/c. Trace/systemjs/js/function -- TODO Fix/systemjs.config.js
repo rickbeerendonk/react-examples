3b2e31ae2b6f38f37414468400a278ceec76dcff
Rick Beerendonk <rick@beerendonk.com>
2019-11-06 12:01:01 -0500

    All "Profiler" examples in one folder.

--- 30 - Profiler/c. Trace/systemjs/js/function -- TODO Fix/systemjs.config.js
+++ 30 - Profiler/c. Trace/systemjs/js/function -- TODO Fix/systemjs.config.js
@@ -14,14 +14,21 @@ SystemJS.config({
   },
   paths: {
     // paths serve as alias
-    'npm:': '../../../../node_modules/'
+    'npm:': '../../../../../node_modules/',
+    'npm-unstable:': '../../../../../node_modules_unstable/'
   },
   map: {
     'plugin-babel': 'npm:systemjs-plugin-babel/plugin-babel.js',
     'systemjs-babel-build':
       'npm:systemjs-plugin-babel/systemjs-babel-browser.js',
     react: 'npm:react/umd/react.development.js',
-    'react-dom': 'npm:react-dom/umd/react-dom.development.js'
+    //'react-cache': 'npm:react-cache/umd/react-cache.development.js',
+    'react-cache': 'npm-unstable:react-cache/umd/react-cache.development.js',
+    'react-dom': 'npm:react-dom/umd/react-dom.development.js',
+    //scheduler: 'npm:scheduler/umd/scheduler.development.js'
+    scheduler: 'npm-unstable:scheduler/cjs/scheduler.development.js',
+    'scheduler/tracing':
+      'npm-unstable:scheduler/cjs/scheduler-tracing.development.js'
   },
   packages: {
     '.': {
