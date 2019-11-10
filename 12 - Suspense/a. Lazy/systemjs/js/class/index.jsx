418caeef5e9eb464084a59ddbe5c42eca99770db
Rick Beerendonk <rick@beerendonk.com>
2019-11-10 10:22:55 +0100

    All "Suspense" examples in one folder. Reorder.

---
+++ 12 - Suspense/a. Lazy/systemjs/js/class/index.jsx
@@ -0,0 +1,9 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.createRoot(document.getElementById('app')).render(<App />);
