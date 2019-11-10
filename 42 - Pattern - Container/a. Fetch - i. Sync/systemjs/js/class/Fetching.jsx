ff92ee70a3e859447fb6fcb7ea3447025dd0e3d5
Rick Beerendonk <rick@beerendonk.com>
2019-11-10 09:36:36 +0100

    All "Pattern - Container - Fetch" examples in one folder.

---
+++ 42 - Pattern - Container/a. Fetch - i. Sync/systemjs/js/class/Fetching.jsx
@@ -0,0 +1,19 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import React from 'react';
+
+function Fetching() {
+  return (
+    <React.Fragment>
+      <img
+        src="../../../../../resources/oblicum-square.svg"
+        className="oblicum-spinner"
+        alt="Fetching..."
+      />
+      Fetching...
+    </React.Fragment>
+  );
+}
+
+export default Fetching;
