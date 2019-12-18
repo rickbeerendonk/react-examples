fc2b08babac0ff0a70d9150fcea558bd41d6889b
Rick Beerendonk <rick@beerendonk.com>
2019-12-18 14:55:06 +0100

    Add "Exceptions - Error Boundaries - Multiple levels" examples.

---
+++ 14 - Exceptions/b. Error Boundaries - iii. Multiple levels/systemjs/js/function/index.jsx
@@ -0,0 +1,18 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import ErrorBoundary from './ErrorBoundary';
+import GreetingEditor from './GreetingEditor';
+import Other from './Other';
+
+ReactDOM.render(
+  <ErrorBoundary>
+    <Other>
+      <GreetingEditor />
+    </Other>
+  </ErrorBoundary>,
+  document.getElementById('app')
+);
