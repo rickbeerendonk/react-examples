3dc904ef77681c2d377ee3ef88fba63612d55e8a
Rick Beerendonk <rick@beerendonk.com>
2019-12-19 23:53:00 +0100

    Remove old experimental functionality.

--- 15 - Suspense/c. createResource (EXPERIMENTAL)/systemjs -- TODO FIX/js/class/index.jsx
+++
@@ -1,9 +0,0 @@
-/*! European Union Public License version 1.2 !*/
-/*! Copyright © 2018 Rick Beerendonk          !*/
-
-import React from 'react';
-import ReactDOM from 'react-dom';
-
-import App from './App';
-
-ReactDOM.createRoot(document.getElementById('app')).render(<App />);
