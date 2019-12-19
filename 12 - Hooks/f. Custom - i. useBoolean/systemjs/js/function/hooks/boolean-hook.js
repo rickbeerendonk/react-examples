849d7c5340a795d51c78030be43fad8e5173e19c
Rick Beerendonk <rick@beerendonk.com>
2019-12-19 05:38:10 +0100

    Add "Hooks - Custom - useBoolean" example.

---
+++ 12 - Hooks/f. Custom - i. useBoolean/systemjs/js/function/hooks/boolean-hook.js
@@ -0,0 +1,20 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import React from 'react';
+
+function useBoolean(initialValue) {
+  const [value, setValue] = React.useState(initialValue);
+
+  const toggle = React.useCallback(() => {
+    setValue(v => !v);
+  }, [setValue]);
+
+  // DevTools state
+  React.useDebugValue(value);
+
+  // Return current state & toggle function
+  return [value, toggle];
+}
+
+export default useBoolean;
