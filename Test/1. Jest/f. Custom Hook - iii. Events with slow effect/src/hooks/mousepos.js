4053ba93f0443aa316d000e98a3da95b6aa33b5f
Rick Beerendonk <rick@beerendonk.com>
2019-12-20 13:48:47 +0100

    Add "Test - Jest - Custom Hook - Events with slow effect" example.

---
+++ Test/1. Jest/f. Custom Hook - iii. Events with slow effect/src/hooks/mousepos.js
@@ -0,0 +1,29 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import React from 'react';
+
+function useMousePosition() {
+  const [mousePosition, setMousePosition] = React.useState({
+    x: undefined,
+    y: undefined
+  });
+
+  React.useEffect(() => {
+    function handleMouseOver(e) {
+      setMousePosition({ x: e.pageX, y: e.pageY });
+    }
+    setTimeout(
+      () => document.addEventListener('mousemove', handleMouseOver),
+      2000
+    );
+    return () => {
+      document.removeEventListener('mousemove', handleMouseOver);
+    };
+  }, [setMousePosition]);
+
+  // Return current state
+  return mousePosition;
+}
+
+export default useMousePosition;
