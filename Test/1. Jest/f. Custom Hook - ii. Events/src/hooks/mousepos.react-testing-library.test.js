2c7ed6e67e6ecb7988f2ce3a964b48bb0e81e433
Rick Beerendonk <rick@beerendonk.com>
2019-12-20 11:24:25 +0100

    Add "Test - Jest - Custom Hook - Events" example.

---
+++ Test/1. Jest/f. Custom Hook - ii. Events/src/hooks/mousepos.react-testing-library.test.js
@@ -0,0 +1,30 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import { renderHook } from '@testing-library/react-hooks';
+import { act } from 'react-test-renderer';
+
+import useMousePosition from './mousepos';
+
+/// React Testing Library ///
+
+describe('useMousePosition', () => {
+  it('mouse move should update the value', async () => {
+    const { result /*, waitForNextUpdate */ } = renderHook(() =>
+      useMousePosition()
+    );
+
+    expect(result.current).toEqual({ x: undefined, y: undefined });
+
+    // Mouse move
+    act(() => {
+      const doc = window.document;
+      let mouseMove = new Event('mousemove');
+      mouseMove.pageX = 10;
+      mouseMove.pageY = 20;
+      doc.dispatchEvent(mouseMove);
+    });
+
+    expect(result.current).toEqual({ x: 10, y: 20 });
+  });
+});
