4b674953f995b8790de81389d97fc8f68080b836
Rick Beerendonk <rick@beerendonk.com>
2020-02-15 15:30:05 +0100

    Add "Component Lifecyle - Unmount when page closed by user" function example.

---
+++ 11 - Component Lifecycle/b. Unmount when page closed by user/systemjs/js/function/index.jsx
@@ -0,0 +1,16 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018-2020 Rick Beerendonk          !*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Parent from './Parent';
+
+const node = document.getElementById('app');
+
+ReactDOM.render(<Parent />, node);
+
+// Add this to get componentWillUnmount / useEffect-cleanup events.
+window.addEventListener('beforeunload', () =>
+  ReactDOM.unmountComponentAtNode(node)
+);
