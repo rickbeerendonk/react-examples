1ccd83c2b0e2f9af269bbb09866e3ce56cf50d62
Rick Beerendonk <rick@beerendonk.com>
2020-02-22 20:47:38 +0100

    Add Storybook docs addon.

--- Storybook/01 - Setup - b. Dynamic loading/.storybook/main.js
+++ Storybook/01 - Setup - b. Dynamic loading/.storybook/main.js
@@ -2,9 +2,10 @@
 /*! Copyright © 2018 Rick Beerendonk          !*/
 
 module.exports = {
-  stories: ['../src/**/*.stories.[tj]s'],
+  stories: ['../src/**/*.stories.(js|ts|mdx)'],
   addons: [
     '@storybook/addon-actions/register',
+    '@storybook/addon-docs',
     '@storybook/addon-knobs/register'
   ]
 };
