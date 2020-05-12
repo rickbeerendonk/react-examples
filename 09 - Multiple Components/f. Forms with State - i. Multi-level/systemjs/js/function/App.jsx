fae87849c4229e8b73f341a736f0482acdfe3ed7
Rick Beerendonk <rick@beerendonk.com>
2020-05-13 00:00:35 +0200

    Add "Multiple Components - Forms with State - Multi-level" example.

---
+++ 09 - Multiple Components/f. Forms with State - i. Multi-level/systemjs/js/function/App.jsx
@@ -0,0 +1,31 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import React from 'react';
+
+import LocationForm from './LocationForm';
+
+function App() {
+  const [list, setList] = React.useState([]);
+
+  const add = React.useCallback(
+    location => {
+      // Create a new list by coping the old and adding the new item
+      setList(l => [...l, location]);
+    },
+    [setList]
+  );
+
+  return (
+    <React.Fragment>
+      <LocationForm onAdd={add} />
+      <ul>
+        {list.map((location, idx) => (
+          <li key={idx}>{JSON.stringify(location)}</li>
+        ))}
+      </ul>
+    </React.Fragment>
+  );
+}
+
+export default App;
