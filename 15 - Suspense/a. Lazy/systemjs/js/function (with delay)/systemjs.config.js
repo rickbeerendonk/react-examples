69065e40cc37ef0075f87d28c99ece82778e4f03
Rick Beerendonk <rick@beerendonk.com>
2020-05-13 21:43:10 +0200

    Add delay to show effect.

---
+++ 15 - Suspense/a. Lazy/systemjs/js/function (with delay)/systemjs.config.js
@@ -0,0 +1,35 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+/* global SystemJS */
+
+SystemJS.config({
+  meta: {
+    '*.jsx': {
+      babelOptions: {
+        es2015: true,
+        react: true
+      }
+    }
+  },
+  paths: {
+    // paths serve as alias
+    'npm:': '../../../../../node_modules/'
+  },
+  map: {
+    'plugin-babel': 'npm:systemjs-plugin-babel/plugin-babel.js',
+    'systemjs-babel-build':
+      'npm:systemjs-plugin-babel/systemjs-babel-browser.js',
+    react: 'npm:react-0.0.0-experimental/umd/react.development.js',
+    'react-dom':
+      'npm:react-dom-0.0.0-experimental/umd/react-dom.development.js',
+    //scheduler: 'npm:scheduler/umd/scheduler.development.js'
+    scheduler: 'npm:scheduler/cjs/scheduler.development.js'
+  },
+  packages: {
+    '.': {
+      defaultExtension: 'jsx'
+    }
+  },
+  transpiler: 'plugin-babel'
+});
