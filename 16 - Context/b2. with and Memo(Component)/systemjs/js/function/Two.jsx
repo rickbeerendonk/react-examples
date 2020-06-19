4800bfc0fc9d3c427f91157f9988ab05030c7546
Rick Beerendonk <rick@beerendonk.com>
2020-06-19 15:06:03 +0200

    Add Function Component version.

---
+++ 16 - Context/b2. with and Memo(Component)/systemjs/js/function/Two.jsx
@@ -0,0 +1,12 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import React from 'react';
+
+import Three from './Three';
+
+function Two() {
+  return <Three />;
+}
+
+export default React.memo(Two);
