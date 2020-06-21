deba60bbcde6fc7d439267e10075f0cea580773c
Rick Beerendonk <rick@beerendonk.com>
2020-06-21 17:59:34 +0200

    Add function version.

---
+++ 13 - DOM/c2. Callback Refs - Performance problem - Inline called twice/systemjs/js/function/index.jsx
@@ -0,0 +1,9 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Hello from './Hello';
+
+ReactDOM.render(<Hello />, document.getElementById('app'));
