d0e7fa205e584b836c2edd6aa2235717cfcffef3
Rick Beerendonk <rick@beerendonk.com>
2020-08-12 12:14:51 +0200

    Add arrow function component examples.

---
+++ 02 - Render Component/b. One element/systemjs/js/function-arrow/Greeting.jsx
@@ -0,0 +1,8 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import React from 'react';
+
+const Greeting = () => <h1>Hello World!</h1>;
+
+export default Greeting;
