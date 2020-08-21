747064bd78f86bfb70bd099858fa60c1bd05219f
Rick Beerendonk <rick@beerendonk.com>
2020-08-21 10:47:41 +0200

    Add "DOM - Callback Refs - Async" example.

---
+++ 13 - DOM/c3. Callback Refs - Async/systemjs/js/function/index.jsx
@@ -0,0 +1,9 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import HelloWorld from './HelloWorld';
+
+ReactDOM.render(<HelloWorld />, document.getElementById('app'));
