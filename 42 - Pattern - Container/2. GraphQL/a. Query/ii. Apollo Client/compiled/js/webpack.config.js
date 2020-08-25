7dd4d28438b6ed12cfacf7367c24c882d9468830
Rick Beerendonk <rick@beerendonk.com>
2020-08-25 23:10:43 +0200

    Regroup.

---
+++ 42 - Pattern - Container/2. GraphQL/a. Query/ii. Apollo Client/compiled/js/webpack.config.js
@@ -0,0 +1,47 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+/* eslint-disable */
+
+const path = require('path');
+const webpack = require('webpack');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: ['./src/index.jsx'],
+  output: {
+    path: path.join(__dirname, '/build'),
+    filename: '[name].[hash:8].js'
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: './build',
+    overlay: true,
+    port: 9100,
+    stats: 'errors-only'
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  plugins: [
+    new CopyWebpackPlugin({ patterns: [{ from: 'public' }] }),
+    new HtmlWebpackPlugin({
+      title: 'Pattern Container: GraphQL Query Apollo Client'
+    }),
+    new webpack.NamedModulesPlugin()
+  ],
+  resolve: {
+    alias: {
+      'react-dom': '@hot-loader/react-dom'
+    },
+    extensions: ['.js', '.jsx']
+  }
+};
