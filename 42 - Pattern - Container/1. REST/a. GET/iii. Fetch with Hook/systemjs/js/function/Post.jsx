0cd212831493fe0f1e9d560df627f7fa0119f4d9
Rick Beerendonk <rick@beerendonk.com>
2020-08-26 23:34:17 +0200

    Add "Pattern - Container - REST - GET - Fetch with Hook" example.

---
+++ 42 - Pattern - Container/1. REST/a. GET/iii. Fetch with Hook/systemjs/js/function/Post.jsx
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import React from 'react';
+
+function Post({ title }) {
+  return <li>{title}</li>;
+}
+
+export default Post;
---
+++ 42 - Pattern - Container/1. REST/a. GET/iii. Fetch with Hook/systemjs/js/function/PostListContainer.jsx
@@ -0,0 +1,54 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import React from 'react';
+import { fetch } from 'slow-fetch';
+
+import ErrorMessage from './ErrorMessage';
+import Fetching from './Fetching';
+import PostList from './PostList';
+
+function useFetchJson(url) {
+  const [json, setJson] = React.useState(undefined);
+  const [error, setError] = React.useState(undefined);
+  const [isFetching, setIsFetching] = React.useState(false);
+
+  React.useEffect(
+    /* Wrap async call so no Promise is returned */
+    function fetchPosts() {
+      (async () => {
+        setIsFetching(true);
+        try {
+          const response = await fetch(url);
+          if (!response.ok) {
+            throw Error(response.statusText);
+          }
+          const json = await response.json();
+          setJson(json);
+        } catch (error) {
+          setError(error.message);
+        } finally {
+          setIsFetching(false);
+        }
+      })();
+    },
+    [url] /* Do effect only once. Set functions of useState never change.  */
+  );
+
+  return [json, error, isFetching];
+}
+
+// Container pattern:
+// Container fetches data, then renders the sub-component.
+function PostListContainer() {
+  const [posts = [], error, isFetching] = useFetchJson('posts.json');
+
+  return (
+    <React.Fragment>
+      {isFetching && <Fetching />}
+      {error ? <ErrorMessage message={error} /> : <PostList posts={posts} />}
+    </React.Fragment>
+  );
+}
+
+export default PostListContainer;
