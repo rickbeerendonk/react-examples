53ca11005d4fe4ae38ff6c1b63a6f9a78b681d2c
Rick Beerendonk <rick@beerendonk.com>
2020-09-05 22:13:22 +0200

    Add TypeScript version.

---
+++ 06 - Events/c. useCallback/systemjs/ts/function/GreetingEditor.tsx
@@ -0,0 +1,34 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import React from 'react';
+
+const handlers = new Set();
+
+const GreetingEditor: React.FC = () => {
+  const [name, setName] = React.useState('World');
+
+  const changeHandler = function (e: React.ChangeEvent<HTMLInputElement>) {
+    setName(e.target.value);
+  };
+
+  // const changeHandler = React.useCallback(
+  // function (e: React.ChangeEvent<HTMLInputElement>) {
+  // setName(e.target.value);
+  // },
+  // [setName]
+  // );
+
+  // Add handler to the set
+  handlers.add(changeHandler);
+
+  return (
+    <React.Fragment>
+      <input autoFocus onChange={changeHandler} value={name} />
+      <h1>Hello {name}!</h1>
+      <div>Number of handlers used: {handlers.size}</div>
+    </React.Fragment>
+  );
+};
+
+export default GreetingEditor;
