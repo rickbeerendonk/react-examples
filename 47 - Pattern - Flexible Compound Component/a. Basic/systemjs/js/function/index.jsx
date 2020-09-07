8fedfb26068c89c6b6f100d841802e7980119430
Rick Beerendonk <rick@beerendonk.com>
2020-09-07 15:50:21 +0200

    Add "Pattern - Flexible Compound Component - Basic" example.

---
+++ 47 - Pattern - Flexible Compound Component/a. Basic/systemjs/js/function/index.jsx
@@ -0,0 +1,17 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import GreetingEditor from './GreetingEditor';
+
+ReactDOM.render(
+  <GreetingEditor>
+    <div style={{ backgroundColor: 'lightgray' }}>
+      <GreetingEditor.Text />
+    </div>
+    <GreetingEditor.Input />
+  </GreetingEditor>,
+  document.getElementById('app')
+);
---
+++ 47 - Pattern - Flexible Compound Component/readme.md
@@ -0,0 +1,11 @@
+# Pattern - Flexible Compound Component
+
+Compared to the Compound Component pattern, sub-components no longer need to be direct children.
+
+## Licence
+
+European Union Public Licence version 1.2
+
+## Copyright
+
+Copyright © 2020 Rick Beerendonk
