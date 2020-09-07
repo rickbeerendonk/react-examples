23bc3e8eeb2bffd91ad1addab079001644049a40
Rick Beerendonk <rick@beerendonk.com>
2020-09-07 22:39:40 +0200

    Add "Flexible Compound Component - Default" example.

---
+++ 47 - Pattern - Flexible Compound Component/b. Default/systemjs/js/function/index.jsx
@@ -0,0 +1,12 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import GreetingEditor from './GreetingEditor';
+
+ReactDOM.render(
+  <GreetingEditor></GreetingEditor>,
+  document.getElementById('app')
+);
