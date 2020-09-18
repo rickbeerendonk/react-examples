c6559ce496d943f99de6607aed59c71567857aa3
Rick Beerendonk <rick@beerendonk.com>
2020-09-18 21:02:39 +0200

    Add runtime/mjs/jsx version.

---
+++ 01 - Render Root/a. Current/i. Use existing DOM element/runtime/mjs/jsx/index.mjs
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2014 Rick Beerendonk          !*/
+
+import React from './react.development.mjs';
+import ReactDOM from './react-dom.development.mjs';
+
+ReactDOM.render(
+  <h1>Hello React {React.version}!</h1>,
+  document.getElementById('app')
+);
