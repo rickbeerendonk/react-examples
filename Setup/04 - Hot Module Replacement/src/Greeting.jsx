0acfa9fc6db16022b853efbd322b6283f2024ee0
Rick Beerendonk <rick@beerendonk.com>
2020-12-16 13:35:21 +0100

    Use new JSX transform.

--- Setup/04 - Hot Module Replacement/src/Greeting.jsx
+++ Setup/04 - Hot Module Replacement/src/Greeting.jsx
@@ -1,8 +1,6 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2017 Rick Beerendonk          !*/
 
-import React from 'react';
-
 import './Greeting.css';
 
 function Greeting({ name }) {
--- Setup/04 - Hot Module Replacement/src/GreetingEditor.jsx
+++ Setup/04 - Hot Module Replacement/src/GreetingEditor.jsx
@@ -1,13 +1,13 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2017 Rick Beerendonk          !*/
 
-import React from 'react';
+import { Component } from 'react';
 import { hot } from 'react-hot-loader/root';
 
 import EditBox from './EditBox';
 import Greeting from './Greeting';
 
-class GreetingEditor extends React.Component {
+class GreetingEditor extends Component {
   constructor(props) {
     super(props);
     this.state = { name: 'World' };
@@ -19,10 +19,10 @@ class GreetingEditor extends React.Component {
   }
   render() {
     return (
-      <React.Fragment>
+      <>
         <EditBox onChange={this.handleChange} name={this.state.name} />
         <Greeting name={this.state.name} />
-      </React.Fragment>
+      </>
     );
   }
 }
