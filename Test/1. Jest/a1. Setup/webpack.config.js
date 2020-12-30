7c854d94741385dd5ce3217802b69a4153b6ba3c
Rick Beerendonk <rick@beerendonk.com>
2020-12-30 16:35:41 +0100

    Remove deprecated config.

--- Test/1. Jest/a1. Setup/webpack.config.js
+++ Test/1. Jest/a1. Setup/webpack.config.js
@@ -4,7 +4,6 @@
 /* eslint-disable */
 
 const path = require('path');
-const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
@@ -43,9 +42,11 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'React Tests with Jest - Setup'
-    }),
-    new webpack.NamedModulesPlugin()
+    })
   ],
+  optimization: {
+    moduleIds: 'named'
+  },
   resolve: {
     extensions: ['.js', '.jsx', '.json']
   }
