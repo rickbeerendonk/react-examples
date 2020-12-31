cb2d5c8a68c73d3bf95be803594ac4baafc4c228
Rick Beerendonk <rick@beerendonk.com>
2020-12-31 09:37:38 +0100

    Add "JSX - Data Binding - Component Type" example.

---
+++ JSX/d. Data Binding - Component Type/systemjs/js/no-jsx/Greeting.jsx
@@ -0,0 +1,11 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import React from 'react';
+
+function Greeting() {
+  const type = 'h1';
+  return React.createElement(type, undefined, 'Hello World!');
+}
+
+export default Greeting;
