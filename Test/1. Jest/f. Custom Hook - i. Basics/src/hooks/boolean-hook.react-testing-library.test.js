21d8275d6831684f438bb3f1b8727788a5b838db
Rick Beerendonk <rick@beerendonk.com>
2021-01-17 14:21:03 +0100

    Fix test projects.

--- Test/1. Jest/f. Custom Hook - i. Basics/src/hooks/boolean-hook.react-testing-library.test.js
+++ Test/1. Jest/f. Custom Hook - i. Basics/src/hooks/boolean-hook.react-testing-library.test.js
@@ -1,6 +1,7 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2019 Rick Beerendonk          !*/
 
+import 'regenerator-runtime/runtime.js';
 import { renderHook } from '@testing-library/react-hooks';
 import { act } from 'react-test-renderer';
 
