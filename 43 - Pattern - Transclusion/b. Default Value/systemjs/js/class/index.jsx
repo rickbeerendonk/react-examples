4398107d50b449781d8f144827f5a45bdad82a60
Rick Beerendonk <rick@beerendonk.com>
2021-01-18 01:07:02 +0100

    Add "Transclusion - Default Value" example.

---
+++ 43 - Pattern - Transclusion/b. Default Value/systemjs/js/class/index.jsx
@@ -0,0 +1,9 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Container from './Container';
+
+ReactDOM.render(<Container></Container>, document.getElementById('app'));
