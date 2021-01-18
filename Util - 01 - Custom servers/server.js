9493600a9db298b0d5a9e0266a593a742f0a3276
Rick Beerendonk <rick@beerendonk.com>
2021-01-18 01:07:47 +0100

    Add Webpack server support.

--- Util - 01 - Custom servers/server.js
+++ Util - 01 - Custom servers/server.js
@@ -5,16 +5,61 @@
 /* eslint-disable no-console */
 
 const childProcess = require('child_process');
+const fs = require('fs');
 const path = require('path');
 
 const [, , portHttp = 8080, filePath] = process.argv;
+
+/*** Helper functions ***/
+
+function findFileInPath(directory, file) {
+  const filename = path.join(directory, file);
+  if (fs.existsSync(filename)) {
+    return filename;
+  } else {
+    const basename = path.dirname(directory);
+    return basename && basename !== directory
+      ? findFileInPath(basename, file)
+      : null;
+  }
+}
+
+/*** Main functions ***/
+
+/// Is Webpack project? ///
+
+if (filePath && fs.statSync(filePath).isDirectory()) {
+  const webpackConfigNames = ['webpack.config.js', 'webpack.dev.js'];
+  const webpackConfigPath = webpackConfigNames.reduce(
+    (path, name) => path || findFileInPath(filePath, name),
+    null
+  );
+  if (webpackConfigPath) {
+    // Webpack config exists, so execute webpack
+    console.log('webpack');
+    console.log('webpack config: ' + webpackConfigPath);
+    childProcess.spawnSync(
+      'webpack-dev-server',
+      ['--config', `"${webpackConfigPath}"`, '--open', '--port', portHttp],
+      {
+        cwd: path.dirname(webpackConfigPath),
+        shell: true,
+        stdio: 'inherit'
+      }
+    );
+    console.log('webpack done');
+    process.exit();
+  }
+}
+
+/// No Webpack ///
+
 const basePath = process.cwd();
 
 // If filePath is given, we'll open that path in the browser
 let extraUri = '';
 if (filePath && filePath.toLowerCase().startsWith(basePath.toLowerCase())) {
-  const extraPath = filePath.substring(basePath.length);
-  extraUri = extraPath.split('\\').join('/');
+  extraUri = filePath.substring(basePath.length).split('\\').join('/');
 }
 
 const serverUri = `http://localhost:${portHttp}`;
@@ -23,8 +68,7 @@ const totalUri = `${serverUri}${extraUri}${
 }`.replace(/\s+/g, '%20');
 
 // Start server
-// See: https://www.npmjs.com/package/serve
-/* const httpChild = */ childProcess.spawn(
+childProcess.spawn(
   'serve',
   ['--listen', portHttp, '--config', `"${path.join(__dirname, 'serve.json')}"`],
   { shell: true, stdio: 'inherit' }
@@ -36,15 +80,10 @@ setTimeout(function () {
   const command =
     process.platform === 'win32'
       ? `start "" "${totalUri}"`
-      : /* process.platform === "darwin" */ `open "${totalUri}"`;
+      : `open "${totalUri}"`;
 
-  /*const browserChild = */ childProcess.spawn(command, {
+  childProcess.spawn(command, {
     shell: true,
     stdio: 'inherit'
   });
-
-  //browserChild.on('close', () => {
-  //  console.log('close');
-  //  process.exit();
-  //});
 }, 200);
