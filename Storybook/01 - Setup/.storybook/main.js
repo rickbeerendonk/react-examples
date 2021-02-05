c403b13cd31d3a53f6ce12f6e64cc93a8d055308
Rick Beerendonk <rick@beerendonk.com>
2021-02-05 08:57:01 +0100

    Shorter syntax.

--- Storybook/01 - Setup/.storybook/main.js
+++ Storybook/01 - Setup/.storybook/main.js
@@ -1,4 +1,4 @@
 module.exports = {
-  stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../src/**/*.stories.@(js|jsx|mdx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials']
 };
