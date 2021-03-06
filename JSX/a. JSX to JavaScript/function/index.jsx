f7ad4a9a45da037a30375ece743e8ae85a838ea5
Rick Beerendonk <rick@beerendonk.com>
2021-03-06 21:57:53 +0100

    Fix comment.

--- JSX/a. JSX to JavaScript/function/index.jsx
+++ JSX/a. JSX to JavaScript/function/index.jsx
@@ -1,5 +1,5 @@
 /*! European Union Public License version 1.2 !*/
-/*! Copyright © 2017-2020 Rick Beerendonk          !*/
+/*! Copyright © 2017-2020 Rick Beerendonk     !*/
 
 /* global Babel, React, ReactDOM */
 
