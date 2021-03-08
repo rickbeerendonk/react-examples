cad70ca5ea7869804d7fcaf0514aac5cc4fb3260
Rick Beerendonk <rick@beerendonk.com>
2021-03-08 09:47:57 +0100

    Add "Data Binding - Data to View - Content" example.

---
+++ 03 - Data Binding/1. Data to View/a. Content/systemjs/js/function/App.jsx
@@ -0,0 +1,12 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import React from 'react';
+
+function App() {
+  const name = 'React';
+
+  return <h1>Hello {name}!</h1>;
+}
+
+export default App;
