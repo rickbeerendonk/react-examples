2c7f662d7d153f93c08ad3e6a69b07cff1916aec
Rick Beerendonk <rick@beerendonk.com>
2021-03-10 10:44:54 +0100

    Add more versions.

---
+++ 03 - Data Binding/1. Data to View/a. Content/runtime/js/jsx/index.jsx
@@ -0,0 +1,12 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+/* global React, ReactDOM */
+
+function App() {
+  const name = 'React';
+
+  return <h1>Hello {name}!</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
---
+++ 03 - Data Binding/1. Data to View/a. Content/runtime/mjs/jsx/index.mjs
@@ -0,0 +1,13 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import React from './react.development.mjs';
+import ReactDOM from './react-dom.development.mjs';
+
+function App() {
+  const name = 'React';
+
+  return <h1>Hello {name}!</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
