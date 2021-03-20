965b50b3836221eea02d67b1b6f40d3d1f634517
Rick Beerendonk <rick@beerendonk.com>
2021-03-20 13:37:56 +0100

    Add class versions.

---
+++ 03 - Data Binding/1. Data to View/a. Content/systemjs/ts/class/App.tsx
@@ -0,0 +1,14 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import React from 'react';
+
+class App extends React.Component {
+  render() {
+    const name = 'React';
+
+    return <h1>Hello {name}!</h1>;
+  }
+}
+
+export default App;
