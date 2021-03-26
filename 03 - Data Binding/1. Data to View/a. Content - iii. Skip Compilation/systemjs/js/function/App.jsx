280b70cd2bdca31d26b69fe84681ce58a482200e
Rick Beerendonk <rick@beerendonk.com>
2021-03-27 00:01:50 +0100

    Add "Data Binding - Data to View - Content - Skip Compilation" example.

---
+++ 03 - Data Binding/1. Data to View/a. Content - iii. Skip Compilation/systemjs/js/function/App.jsx
@@ -0,0 +1,15 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import React from 'react';
+
+function App() {
+  return (
+    <>
+      <h1>Hello {'{name}'}!</h1>
+      <h1>Hello &#123;name&#125;!</h1>
+    </>
+  );
+}
+
+export default App;
