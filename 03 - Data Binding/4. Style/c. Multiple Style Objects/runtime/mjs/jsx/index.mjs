7a9cef94cffe2e11f2e9982c9d60b8cdc66037ca
Rick Beerendonk <rick@beerendonk.com>
2021-04-02 16:43:25 +0200

    Add "Data Binding - Style - Multiple Style Objects" example.

---
+++ 03 - Data Binding/4. Style/c. Multiple Style Objects/runtime/mjs/jsx/index.mjs
@@ -0,0 +1,22 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import React from './react.development.mjs';
+import ReactDOM from './react-dom.development.mjs';
+
+function App() {
+  const style1 = {
+    color: 'blue',
+    fontStyle: 'italic'
+  };
+  const style2 = {
+    color: 'red',
+    textDecoration: 'underline'
+  };
+
+  // Pure JS, nothing specific React.
+  // Some frameworks (like Vue) need special syntax.
+  return <h1 style={{ ...style1, ...style2 }}>Hello React!</h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById('app'));
