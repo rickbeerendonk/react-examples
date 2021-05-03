0c6eb94673993f6e43b9478b9069228022b4203b
Rick Beerendonk <rick@beerendonk.com>
2021-05-03 20:38:05 +0200

    Simplification.

---
+++ 04 - Render Conditionally/z. [DO NOT USE] if-else/systemjs/js/function/App.jsx
@@ -0,0 +1,23 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import React from 'react';
+
+function App() {
+  const show = false;
+
+  return (
+    <>
+      <h3>Conditional:</h3>
+      {(function () {
+        if (show) {
+          return <div>Invisible</div>;
+        } else {
+          return <div>Visible</div>;
+        }
+      })()}
+    </>
+  );
+}
+
+export default App;
