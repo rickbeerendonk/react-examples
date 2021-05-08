7683238c787baa6d03a6bc364df34cc1a9da00ef
Rick Beerendonk <rick@beerendonk.com>
2021-05-08 16:21:31 +0200

    Remove unused eslint rules.

--- Debugging/a. Basics/compiled/js/function/src/memo_component-stack/Memo_ComponentStack.jsx
+++ Debugging/a. Basics/compiled/js/function/src/memo_component-stack/Memo_ComponentStack.jsx
@@ -1,8 +1,6 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2021 Rick Beerendonk          !*/
 
-/* eslint no-console:"off" */
-
 import React from 'react';
 
 function Memo_ComponentStack() {
