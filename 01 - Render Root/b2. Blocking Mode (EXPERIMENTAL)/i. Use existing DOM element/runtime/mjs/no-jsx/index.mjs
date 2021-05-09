8090f2b8749da4482e1897ca1967ba281e3bdd29
Rick Beerendonk <rick@beerendonk.com>
2021-05-09 11:52:43 +0200

    Blocking Mode is removed https://github.com/facebook/react/pull/20974

--- 01 - Render Root/b2. Blocking Mode (EXPERIMENTAL)/i. Use existing DOM element/runtime/mjs/no-jsx/index.mjs
+++
@@ -1,9 +0,0 @@
-/*! European Union Public License version 1.2 !*/
-/*! Copyright © 2019 Rick Beerendonk          !*/
-
-import React from './react.development.mjs';
-import ReactDOM from './react-dom.development.mjs';
-
-ReactDOM.unstable_createBlockingRoot(document.getElementById('app')).render(
-  React.createElement('h1', null, 'Hello React', React.version, '!')
-);
