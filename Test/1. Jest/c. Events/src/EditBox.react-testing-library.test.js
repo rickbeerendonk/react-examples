c6fb332b83d9cef2e2480a1598678ed2da061cf0
Rick Beerendonk <rick@beerendonk.com>
2021-05-12 13:45:59 +0200

    Add "react-testing-library" tests.

---
+++ Test/1. Jest/c. Events/src/EditBox.react-testing-library.test.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { fireEvent, render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import EditBox from './EditBox';
+
+/// React Test Renderer ///
+
+describe('EditBox', () => {
+  /// -- Property -- ///
+
+  it('does not throw when no name is provided (react-test-renderer)', () => {
+    expect(() => {
+      render(<EditBox />);
+    }).not.toThrow();
+  });
+
+  it('renders world inline snapshot when value is world (react-test-renderer)', () => {
+    const { container } = render(<EditBox value="World" />);
+
+    expect(container.firstChild).toMatchInlineSnapshot(`
+      <input
+        value="World"
+      />
+    `);
+  });
+
+  it('renders world snapshot when value is world (react-test-renderer)', () => {
+    const { container } = render(<EditBox value="World" />);
+    expect(container.firstChild).toMatchSnapshot();
+  });
+
+  /// -- Event -- ///
+
+  it('raises onChange event when changed (react-test-renderer)', () => {
+    const handleChange = jest.fn();
+    const { container } = render(
+      <EditBox onChange={handleChange} name="World" />
+    );
+
+    // trigger the onChange event for the select box
+    fireEvent.change(container.firstChild, { target: { value: 'React' } });
+
+    expect(handleChange).toHaveBeenCalledTimes(1);
+    expect(handleChange).toHaveBeenCalledWith('React');
+  });
+});
