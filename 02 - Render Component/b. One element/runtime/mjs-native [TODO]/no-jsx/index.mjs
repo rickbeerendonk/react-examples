eaa957992dcde8a4207077a0c1b982fea37867ee
Rick Beerendonk <rick@beerendonk.com>
2021-06-12 14:51:52 +0200

    Add React Native Web TODO.

---
+++ 02 - Render Component/b. One element/runtime/mjs-native [TODO]/no-jsx/index.mjs
@@ -0,0 +1,9 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2014 Rick Beerendonk          !*/
+
+import React from 'react-native-web';
+import ReactDOM from './react-dom.development.mjs';
+
+import Greeting from './Greeting.mjs';
+
+ReactDOM.render(React.createElement(Greeting), document.getElementById('app'));
