4be204c4c37683aaaebe84661a37a9bd0c9f423c
Rick Beerendonk <rick@beerendonk.com>
2021-06-22 00:58:08 +0200

    Short version.

--- Setup/04 - Fast Refresh/js/webpack.config.js
+++ Setup/04 - Fast Refresh/js/webpack.config.js
@@ -39,7 +39,7 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: [{ loader: 'style-loader' }, { loader: 'css-loader' }]
+        use: ['style-loader', 'css-loader']
       }
     ]
   },
