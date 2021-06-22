f2912933b3dcec0835dfdb43a4107d12dfe54cf9
Rick Beerendonk <rick@beerendonk.com>
2021-06-22 23:49:52 +0200

    Add TypeScript version.

---
+++ 05 - Render List/a. Array/systemjs/ts/function/App.tsx
@@ -0,0 +1,24 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import React from 'react';
+
+function App() {
+  const names = ['Alexandra', 'Benjamin', 'Charlotte'];
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <ul>
+        {
+          // Prefer map (instead of for-loops):
+          names.map(name => (
+            <li key={name}>Hello {name}</li>
+          ))
+        }
+      </ul>
+    </>
+  );
+}
+
+export default App;
