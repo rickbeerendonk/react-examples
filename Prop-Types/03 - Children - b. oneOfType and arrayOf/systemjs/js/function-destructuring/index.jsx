6e6050b7f36bf541e4f15eab9cbea3c940e1a837
Rick Beerendonk <rick@beerendonk.com>
2021-08-18 21:59:08 +0200

    Add "Prop-Types - Children - oneOfType and arrayOf" example.

---
+++ Prop-Types/03 - Children - b. oneOfType and arrayOf/systemjs/js/function-destructuring/index.jsx
@@ -0,0 +1,25 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import List from './List';
+
+ReactDOM.render(
+  <div>
+    <List>{/* legal */}</List>
+
+    <List>
+      <span>Legal 1</span>
+    </List>
+
+    <List>
+      <span>Legal 2</span>
+      <span>Legal 3</span>
+    </List>
+
+    <List>{123 /* problem */}</List>
+  </div>,
+  document.getElementById('app')
+);
