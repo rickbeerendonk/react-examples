d7af00a3ae18adace467ecbac4cf08cc8c0306b0
Rick Beerendonk <rick@beerendonk.com>
2021-08-20 15:41:54 +0200

    Add "Suspense List - No List" demo.

---
+++ 80 - Suspense List (React 18)/_. No List/systemjs/js/function/App.jsx
@@ -0,0 +1,42 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import React from 'react';
+
+import Spinner from './Spinner';
+import AsyncResult from './AsyncResult';
+
+function App() {
+  const [buttonClicked, setButtonClicked] = React.useState(false);
+
+  function load() {
+    setButtonClicked({ buttonClicked: true });
+  }
+
+  return (
+    <>
+      <button onClick={load}>Load</button>
+      {buttonClicked && (
+        <ol>
+          <li value="1">
+            <React.Suspense fallback={<Spinner name="one" />}>
+              <AsyncResult name="one" time={2000} />
+            </React.Suspense>
+          </li>
+          <li value="2">
+            <React.Suspense fallback={<Spinner name="two" />}>
+              <AsyncResult name="two" time={3000} />
+            </React.Suspense>
+          </li>
+          <li value="3">
+            <React.Suspense fallback={<Spinner name="three" />}>
+              <AsyncResult name="three" time={1000} />
+            </React.Suspense>
+          </li>
+        </ol>
+      )}
+    </>
+  );
+}
+
+export default App;
