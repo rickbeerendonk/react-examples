d018834fde5ba1d3cfd765dfb4472954fa73bca8
Rick Beerendonk <rick@beerendonk.com>
2021-11-10 20:42:16 +0100

    Add "DOM - Callback Refs - Refresh" example.

---
+++ 13 - DOM/c2. Callback Refs - Refresh/systemjs/js/function/HelloWorld.jsx
@@ -0,0 +1,26 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import React from 'react';
+
+function HelloWorld() {
+  const [id, setId] = React.useState(0);
+
+  function handleRef(ref) {
+    console.log(ref);
+    if (ref) {
+      ref.innerHTML = 'Hello <u>React</u>!!!';
+    }
+  }
+
+  return (
+    <>
+      <h1 ref={handleRef} key={id}>
+        Hello World!
+      </h1>
+      <button onClick={() => setId(v => v + 1)}>Refresh</button>
+    </>
+  );
+}
+
+export default HelloWorld;
