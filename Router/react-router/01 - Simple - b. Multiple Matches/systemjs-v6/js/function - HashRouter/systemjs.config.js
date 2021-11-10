dd3abd1d01acbc602f5fccb14d72898d435aac61
Rick Beerendonk <rick@beerendonk.com>
2021-11-10 21:23:38 +0100

    Add v6 version.

---
+++ Router/react-router/01 - Simple - b. Multiple Matches/systemjs-v6/js/function - HashRouter/systemjs.config.js
@@ -0,0 +1,36 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+/* global SystemJS */
+
+SystemJS.config({
+  meta: {
+    '*.jsx': {
+      babelOptions: {
+        es2015: true,
+        react: true
+      }
+    }
+  },
+  paths: {
+    // paths serve as alias
+    'npm:': '../../../../../../node_modules/'
+  },
+  map: {
+    history: 'npm:history/umd/history.development.js',
+    'plugin-babel': 'npm:systemjs-plugin-babel/plugin-babel.js',
+    'systemjs-babel-build':
+      'npm:systemjs-plugin-babel/systemjs-babel-browser.js',
+    react: 'npm:react/umd/react.development.js',
+    'react-dom': 'npm:react-dom/umd/react-dom.development.js',
+    'react-router': 'npm:react-router-6/umd/react-router.development.js',
+    'react-router-dom':
+      'npm:react-router-dom-6/umd/react-router-dom.development.js'
+  },
+  packages: {
+    '.': {
+      defaultExtension: 'jsx'
+    }
+  },
+  transpiler: 'plugin-babel'
+});
