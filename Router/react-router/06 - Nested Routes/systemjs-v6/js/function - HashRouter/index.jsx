ac6f731d2b7a4f5ac36bd554fdb08fd15217ec79
Rick Beerendonk <rick@beerendonk.com>
2021-11-11 02:12:29 +0100

    Add v6 version.

---
+++ Router/react-router/06 - Nested Routes/systemjs-v6/js/function - HashRouter/index.jsx
@@ -0,0 +1,24 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
+
+import App from './App';
+import Home from './routes/Home';
+import Page1 from './routes/Page1';
+import Page2 from './routes/Page2';
+
+ReactDOM.render(
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<Home />} />
+        <Route path="page1/*" element={<Page1 />} />
+        <Route path="page2/*" element={<Page2 />} />
+      </Route>
+    </Routes>
+  </HashRouter>,
+  document.getElementById('root')
+);
