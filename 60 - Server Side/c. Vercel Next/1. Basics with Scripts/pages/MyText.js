ebb09672f7a64694e3a5cc7d270fa64618bbde8f
Rick Beerendonk <rick@beerendonk.com>
2021-12-22 14:31:12 -0500

    Add "Server Side - Next.js - Basics with Scritps" example.

---
+++ 60 - Server Side/c. Vercel Next/1. Basics with Scripts/pages/MyText.js
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2017 Rick Beerendonk          !*/
+
+import React from 'react';
+
+function MyText({ text }) {
+  return <i>{text}</i>;
+}
+
+export default MyText;
---
+++ 60 - Server Side/c. Vercel Next/1. Basics with Scripts/pages/index.js
@@ -0,0 +1,15 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2017 Rick Beerendonk          !*/
+
+import React from 'react';
+import MyText from './MyText.js';
+
+function Index() {
+  return (
+    <h1>
+      Hello <MyText text="Next.js" />!
+    </h1>
+  );
+}
+
+export default Index;
