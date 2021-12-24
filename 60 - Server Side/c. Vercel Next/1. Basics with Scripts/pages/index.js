0d513bc3e9a2734d35fadc43c50ca5118dee3ade
Rick Beerendonk <rick@beerendonk.com>
2021-12-24 15:22:31 -0500

    No React import needed.

--- 60 - Server Side/c. Vercel Next/1. Basics with Scripts/pages/index.js
+++ 60 - Server Side/c. Vercel Next/1. Basics with Scripts/pages/index.js
@@ -1,7 +1,6 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2017 Rick Beerendonk          !*/
 
-import React from 'react';
 import MyText from './MyText.js';
 
 function Index() {
