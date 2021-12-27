f89f1b7cade4c6556172e3f31e1025f52138cdb8
Rick Beerendonk <rick@beerendonk.com>
2021-12-26 21:56:21 -0500

    Add "Server Side - Next.js - Head Title" example.

---
+++ 60 - Server Side/c. Vercel Next/3. CSS - i. Global/pages/_app.js
@@ -0,0 +1,9 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import './global.css';
+
+// This default export is required in a new `pages/_app.js` file.
+export default function MyApp({ Component, pageProps }) {
+  return <Component {...pageProps} />;
+}
