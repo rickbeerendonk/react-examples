06f1fab4a0a5fff6abec0e3b3cd2b5a3e4db0ebc
Rick Beerendonk <rick@beerendonk.com>
2021-12-29 22:23:02 -0500

    Add "Server Side - Next.js - CSS - Local" example.

---
+++ 60 - Server Side/c. Next.js/3. CSS - iI. Local/pages/MyText.js
@@ -0,0 +1,8 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+function MyText({ text }) {
+  return <i>{text}</i>;
+}
+
+export default MyText;
