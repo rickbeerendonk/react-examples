3ede39103339419a75c6b77d87d05b8f5c4ed3b4
Rick Beerendonk <rick@beerendonk.com>
2022-01-01 20:30:33 +0100

    Add "Render Props - For-loop" example.

---
+++ 45 - Pattern - Render Props/z. [Render List] For-loop/systemjs/js/function/For.jsx
@@ -0,0 +1,8 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2022 Rick Beerendonk          !*/
+
+function For({ children, of }) {
+  return of.map(children);
+}
+
+export default For;
