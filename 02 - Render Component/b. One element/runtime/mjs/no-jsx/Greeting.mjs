c8ec8f625e868ef0577078cd9b6225d6457203e6
Rick Beerendonk <rick@beerendonk.com>
2022-01-03 23:12:21 +0100

    Use React ESM bundle.

--- 02 - Render Component/b. One element/runtime/mjs/no-jsx/Greeting.mjs
+++ 02 - Render Component/b. One element/runtime/mjs/no-jsx/Greeting.mjs
@@ -1,7 +1,7 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2014 Rick Beerendonk          !*/
 
-import React from './react.development.mjs';
+import React from 'react';
 
 function Greeting() {
   return React.createElement('h1', null, 'Hello World!');
--- 02 - Render Component/b. One element/runtime/mjs/no-jsx/index.mjs
+++ 02 - Render Component/b. One element/runtime/mjs/no-jsx/index.mjs
@@ -1,8 +1,8 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2014 Rick Beerendonk          !*/
 
-import React from './react.development.mjs';
-import ReactDOM from './react-dom.development.mjs';
+import React from 'react';
+import ReactDOM from 'react-dom';
 
 import Greeting from './Greeting.mjs';
 
