560c35b77c377954853f91e73b300b1567acef42
Rick Beerendonk <rick@beerendonk.com>
2022-01-06 12:42:40 +0100

    Add "React-Spring - useSpring - Number" example.

---
+++ React-Spring/01 - useSpring - a. Number/runtime/mjs/jsx/index.mjs
@@ -0,0 +1,14 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2022 Rick Beerendonk          !*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { useSpring, animated } from 'react-spring';
+
+function App() {
+  const obj = useSpring({ from: { number: 0 }, to: { number: 1 } });
+  return <animated.h1>{obj.number}</animated.h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
