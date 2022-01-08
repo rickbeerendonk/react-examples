e555a275618b55d1c40595f45b0fa073fe53089b
Rick Beerendonk <rick@beerendonk.com>
2022-01-08 22:31:00 +0100

    Add runtime version.

---
+++ React-Spring/01 - Spring - b. Style/runtime/mjs/jsx/index.mjs
@@ -0,0 +1,17 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2022 Rick Beerendonk          !*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { Spring, animated } from 'react-spring';
+
+function App() {
+  return (
+    <Spring from={{ number: 0 }} to={{ number: 1 }}>
+      {props => <animated.h1>{props.number}</animated.h1>}
+    </Spring>
+  );
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
