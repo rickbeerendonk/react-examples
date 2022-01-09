0ecd5eaf55d6f7e95f5eec408162ea81a7a01bdc
Rick Beerendonk <rick@beerendonk.com>
2022-01-09 22:32:08 +0100

    Add "React-Spring - useSpring - Style" example.

---
+++ React-Spring/01 - useSpring - b. Style/runtime/mjs/jsx/index.mjs
@@ -0,0 +1,14 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2022 Rick Beerendonk          !*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { useSpring, animated } from 'react-spring';
+
+function App() {
+  const props = useSpring({ from: { opacity: 0 }, to: { opacity: 1 } });
+  return <animated.h1>{props.number}</animated.h1>;
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
