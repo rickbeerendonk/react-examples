f632b2396db01c8b0acafe7ec1ff91a0d0bb5f9a
Rick Beerendonk <rick@beerendonk.com>
2022-01-15 20:11:30 +0100

    Use StrictMode for cleaner DevTools.

--- Debugging/a. Basics/compiled/js/function/src/index.jsx
+++ Debugging/a. Basics/compiled/js/function/src/index.jsx
@@ -1,6 +1,7 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2017 Rick Beerendonk          !*/
 
+import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
@@ -8,4 +9,9 @@ import App from './App';
 const root = document.createElement('div');
 document.body.appendChild(root);
 
-ReactDOM.render(<App />, root);
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  root
+);
