67c3daa2cd9e9a522cf17f3d54111e3a16b1aaee
Rick Beerendonk <rick@beerendonk.com>
2022-03-09 01:49:21 +0100

    Add v17 vs v18 Batched Updates examples.

---
+++ 07 - State/i1. React 17 - No Batched Updates/systemjs/js/function/index.jsx
@@ -0,0 +1,9 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2022 Rick Beerendonk          !*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
