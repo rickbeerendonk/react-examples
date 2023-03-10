7e72770b9e219d863194aebb6df88956ebea41fa
Rick Beerendonk <rick@beerendonk.com>
2023-03-10 22:49:50 +0100

    Add TypeScript versions.

---
+++ 15 - Suspense/a. Lazy - ii. Named export/systemjs/ts [WIP]/function/App.tsx
@@ -0,0 +1,38 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import React from 'react';
+
+//import { Greeting } from './Greeting';
+
+const Greeting = React.lazy(() =>
+  import('./Greeting').then(module => ({ default: module.Greeting }))
+);
+
+/*
+function superLazy(moduleName, exportName) {
+  return React.lazy(() =>
+    import(moduleName).then(module => ({ default: module[exportName] }))
+  );
+}
+const Greeting = superLazy('./Greeting', 'Greeting');
+*/
+
+function App() {
+  const [visible, setVisible] = React.useState(false);
+
+  return (
+    <React.Fragment>
+      <button onClick={() => setVisible(v => !v)}>
+        {visible ? 'Hide' : 'Show'}
+      </button>
+      {visible && (
+        <React.Suspense fallback={<div>Loading...</div>}>
+          <Greeting />
+        </React.Suspense>
+      )}
+    </React.Fragment>
+  );
+}
+
+export default App;
