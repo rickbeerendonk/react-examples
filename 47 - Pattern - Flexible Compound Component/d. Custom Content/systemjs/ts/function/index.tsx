68127f0c1bbaeb3e1fd43f277aa14990904d5440
Rick Beerendonk <rick@beerendonk.com>
2023-03-12 17:48:18 +0100

    Add TypeScript version.

---
+++ 47 - Pattern - Flexible Compound Component/d. Custom Content/systemjs/ts/function/index.tsx
@@ -0,0 +1,18 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018-2022 Rick Beerendon      !*/
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import CustomContent from './CustomContent';
+import GreetingEditor from './GreetingEditor';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <GreetingEditor>
+    <div style={{ backgroundColor: 'lightgray' }}>
+      <CustomContent />
+    </div>
+    <GreetingEditor.Input />
+  </GreetingEditor>
+);
