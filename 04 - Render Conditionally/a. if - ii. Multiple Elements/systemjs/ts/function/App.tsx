30b7e59245460458775ae5e3280f112c27bef005
Rick Beerendonk <rick@beerendonk.com>
2023-03-13 10:59:30 +0100

    Add TypeScript versions.

---
+++ 04 - Render Conditionally/a. if - ii. Multiple Elements/systemjs/ts/function/App.tsx
@@ -0,0 +1,22 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import React from 'react';
+
+function App() {
+  const show = true;
+
+  return (
+    <>
+      <h3>Conditional:</h3>
+      {show && (
+        <>
+          <div>Visible</div>
+          <div>Also Visible</div>
+        </>
+      )}
+    </>
+  );
+}
+
+export default App;
