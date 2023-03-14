ed875f846775e36ea87abef73047ec8fc2169e5b
Rick Beerendonk <rick@beerendonk.com>
2023-03-14 20:42:26 +0100

    Add TypeScript version.

---
+++ 07 - State/a. fixed updater (onChange)/systemjs/ts/function-useReducer/index.tsx
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import GreetingEditor from './GreetingEditor';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<GreetingEditor />);
