62280f1497e65f358b670aa3bcfc2a018f296ed7
Rick Beerendonk <rick@beerendonk.com>
2023-03-14 21:27:52 +0100

    Update code to latest.

---
+++ 80 - Suspense List (Future)/a. revealOrder/i. Together/systemjs/js/function/systemjs.config.js
@@ -0,0 +1,40 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+/* global SystemJS */
+
+SystemJS.config({
+  paths: {
+    // paths serve as alias
+    'npm:': '../../../../../../node_modules/'
+  },
+  map: {
+    react: 'npm:react-experimental/umd/react.development.js',
+    'react-dom': 'npm:react-dom-experimental/umd/react-dom.development.js',
+    'react-dom/client':
+      'npm:react-dom-experimental/umd/react-dom.development.js',
+
+    ts: 'npm:plugin-typescript',
+    typescript: 'npm:typescript'
+  },
+  packages: {
+    '.': {
+      defaultExtension: 'jsx'
+    },
+    ts: {
+      main: 'lib/plugin.js'
+    },
+    typescript: {
+      main: 'lib/typescript.js',
+      meta: {
+        'lib/typescript.js': {
+          exports: 'ts'
+        }
+      }
+    }
+  },
+  transpiler: 'ts',
+  typescriptOptions: {
+    tsconfig: true
+  }
+});
