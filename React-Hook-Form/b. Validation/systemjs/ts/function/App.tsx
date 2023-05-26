eb976328da7ef24e111f835f62be453a38c5047b
Rick Beerendonk <rick@beerendonk.com>
2023-05-26 19:45:40 +0200

    Add TS version.

---
+++ React-Hook-Form/b. Validation/systemjs/ts/function/App.tsx
@@ -0,0 +1,28 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2023 Rick Beerendonk          !*/
+
+import React from 'react';
+
+import ChildWithForm, { Data } from './ChildWithForm';
+
+function App() {
+  const [list, setList] = React.useState(new Array<Data>());
+
+  const handleAdd = React.useCallback((item: Data) => {
+    // Create a new list by coping the old and adding the new item
+    setList(l => [...l, item]);
+  }, []);
+
+  return (
+    <>
+      <ChildWithForm onAdd={handleAdd} />
+      <ul>
+        {list.map((item, idx) => (
+          <li key={idx}>{JSON.stringify(item)}</li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+export default App;
---
+++ React-Hook-Form/b. Validation/systemjs/ts/function/index.tsx
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2023 Rick Beerendonk     !*/
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
