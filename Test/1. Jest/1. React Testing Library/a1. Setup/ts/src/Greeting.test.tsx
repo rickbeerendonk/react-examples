0837c4db3e718dc2b75919d33664c282a3045719
Rick Beerendonk <rick@beerendonk.com>
2023-05-31 01:41:54 +0200

    Add TS version.

---
+++ Test/1. Jest/1. React Testing Library/a1. Setup/ts/src/Greeting.test.tsx
@@ -0,0 +1,13 @@
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Greeting from './Greeting';
+
+/// React Testing Library ///
+
+describe('Greeting', () => {
+  it('renders hello world snapshot when name is world', () => {
+    const { container } = render(<Greeting name="World" />);
+
+    expect(container.firstChild).toMatchSnapshot();
+  });
+});
