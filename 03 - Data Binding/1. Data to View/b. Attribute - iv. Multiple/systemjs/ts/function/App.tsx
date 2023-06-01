6f5ec1579ca8a195d969c521f2fe7ff069f8e2e1
Rick Beerendonk <rick@beerendonk.com>
2023-06-01 07:44:38 +0200

    Add "Data Binding - Data to View - Attribute - Multiple" example.

---
+++ 03 - Data Binding/1. Data to View/b. Attribute - iv. Multiple/systemjs/ts/function/App.tsx
@@ -0,0 +1,16 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2023 Rick Beerendonk          !*/
+
+import React from 'react';
+
+const App = () => {
+  const attr = {
+    min: 18,
+    max: 65,
+    type: 'number',
+    defaultValue: 25
+  };
+  return <input {...attr}></input>;
+};
+
+export default App;
