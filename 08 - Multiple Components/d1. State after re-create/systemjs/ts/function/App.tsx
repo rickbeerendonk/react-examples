f25569e795806ab2622e7308c8b206e3ee85ef78
Rick Beerendonk <rick@beerendonk.com>
2023-06-28 04:35:26 +0200

    Add TS version.

---
+++ 08 - Multiple Components/d1. State after re-create/systemjs/ts/function/App.tsx
@@ -0,0 +1,27 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import React from 'react';
+
+import ClickMe from './ClickMe';
+
+// Observe that after clicking the button twice,
+// the ClickMe component is recreated with its initial state.
+
+function App() {
+  const [showClickMe, setShowClickMe] = React.useState<boolean>(true);
+
+  function handleClick() {
+    setShowClickMe(scm => !scm);
+  }
+
+  return (
+    <React.Fragment>
+      <button onClick={handleClick}>Change visibility</button>
+      <br />
+      {showClickMe && <ClickMe />}
+    </React.Fragment>
+  );
+}
+
+export default App;
---
+++ 08 - Multiple Components/d1. State after re-create/systemjs/ts/function/ClickMe.tsx
@@ -0,0 +1,19 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import React from 'react';
+
+function ClickMe() {
+  const [count, setCount] = React.useState<number>(0);
+
+  function handleClick() {
+    setCount(c => c + 1);
+  }
+  return (
+    <button onClick={handleClick}>
+      This button has been clicked {count} times
+    </button>
+  );
+}
+
+export default ClickMe;
