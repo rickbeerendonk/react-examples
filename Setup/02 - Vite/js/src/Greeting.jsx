8aa6b2cfab6202e7192262b4408cb146db9e9f0f
Rick Beerendonk <rick@beerendonk.com>
2023-08-12 17:54:53 +0200

    Add "Setup - Vite" examples.

---
+++ Setup/02 - Vite/js/src/Greeting.jsx
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2017 Rick Beerendonk          !*/
+
+import './Greeting.css';
+
+function Greeting() {
+  return <h1 className="greeting">Hello World!</h1>;
+}
+
+export default Greeting;
---
+++ Setup/02 - Vite/ts/src/main.tsx
@@ -0,0 +1,7 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2023 Rick Beerendonk          !*/
+
+import ReactDOM from 'react-dom/client';
+import Greeting from './Greeting.tsx';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<Greeting />);
