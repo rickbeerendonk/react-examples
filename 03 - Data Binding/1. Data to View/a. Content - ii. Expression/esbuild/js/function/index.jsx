7b1856b0bbf1e2b2be2540aaca4ab5d907071430
Rick Beerendonk <rick@beerendonk.com>
2023-08-20 01:52:32 +0200

    Use ESBuild instead of SystemJS.

--- 03 - Data Binding/1. Data to View/a. Content - ii. Expression/esbuild/js/function/index.jsx
+++ 03 - Data Binding/1. Data to View/a. Content - ii. Expression/esbuild/js/function/index.jsx
@@ -4,7 +4,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import App from './App';
+import App from './App.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
--- 03 - Data Binding/1. Data to View/a. Content - ii. Expression/esbuild/ts/class/index.tsx
+++ 03 - Data Binding/1. Data to View/a. Content - ii. Expression/esbuild/ts/class/index.tsx
@@ -1,10 +1,10 @@
 /*! European Union Public License version 1.2 !*/
-/*! Copyright © 2021 Rick Beerendonk     !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
 
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 
-import App from './App';
+import App from './App.tsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as Element);
 root.render(<App />);
