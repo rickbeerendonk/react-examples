2e1d5814f760cb3d661b47d0fa58e2f2a86ebcb6
Rick Beerendonk <rick@beerendonk.com>
2023-09-25 14:37:29 +0200

    Use named exports.

--- 02 - Render Component/c3. Multiple elements (array)/esbuild/js/function/index.jsx
+++ 02 - Render Component/c3. Multiple elements (array)/esbuild/js/function/index.jsx
@@ -1,10 +1,9 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2016 Rick Beerendonk          !*/
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import Greeting from './Greeting.jsx';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 root.render(<Greeting />);
