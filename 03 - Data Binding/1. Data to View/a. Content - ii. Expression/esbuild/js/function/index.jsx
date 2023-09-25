d1b0f0b7fc83020e9f3ab8c2a7c10b68f43a1a18
Rick Beerendonk <rick@beerendonk.com>
2023-09-25 14:49:13 +0200

    Use named exports.

--- 03 - Data Binding/1. Data to View/a. Content - ii. Expression/esbuild/js/function/index.jsx
+++ 03 - Data Binding/1. Data to View/a. Content - ii. Expression/esbuild/js/function/index.jsx
@@ -1,10 +1,9 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2021 Rick Beerendonk          !*/
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 import App from './App.jsx';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = createRoot(document.getElementById('root'));
 root.render(<App />);
