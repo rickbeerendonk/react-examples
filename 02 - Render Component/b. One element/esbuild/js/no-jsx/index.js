62d84bab6a0ba805488d091e5f40e30ad8f731f0
Rick Beerendonk <rick@beerendonk.com>
2023-09-25 14:07:01 +0200

    Use named exports.

--- 02 - Render Component/b. One element/esbuild/js/no-jsx/index.js
+++ 02 - Render Component/b. One element/esbuild/js/no-jsx/index.js
@@ -1,10 +1,10 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2016 Rick Beerendonk          !*/
 
-import React from 'react';
-import ReactDOM from 'react-dom';
+import { createElement } from 'react';
+import { createRoot } from 'react-dom/client';
 
 import Greeting from './Greeting.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(React.createElement(Greeting));
+const root = createRoot(document.getElementById('root'));
+root.render(createElement(Greeting));
