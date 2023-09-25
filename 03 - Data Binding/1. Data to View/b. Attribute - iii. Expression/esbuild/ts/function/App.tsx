cb0b4d86dcff2c585a72c5be689496c93f2a9de6
Rick Beerendonk <rick@beerendonk.com>
2023-09-25 23:19:54 +0200

    Remove unused imports.

--- 03 - Data Binding/1. Data to View/b. Attribute - iii. Expression/esbuild/ts/function/App.tsx
+++ 03 - Data Binding/1. Data to View/b. Attribute - iii. Expression/esbuild/ts/function/App.tsx
@@ -1,8 +1,6 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2021 Rick Beerendonk          !*/
 
-import React from 'react';
-
 const App = () => {
   return (
     <input type="date" value={new Date().toISOString().substring(0, 10)} />
