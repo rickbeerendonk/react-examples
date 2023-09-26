9b70b8e84b0185ade0ff18ff9d9d451679d2f258
Rick Beerendonk <rick@beerendonk.com>
2023-09-26 19:21:39 +0200

    Use ESBuild instead of SystemJS.

---
+++ 03 - Data Binding/4. Style/b. In Code - ii. Individual Values/esbuild/js/function/App.jsx
@@ -0,0 +1,15 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+function App() {
+  const fontSize = 48;
+  const fontStyle = 'italic';
+
+  return (
+    <h1 style={{ fontSize, fontStyle, textDecoration: 'underline' }}>
+      Hello React!
+    </h1>
+  );
+}
+
+export default App;
---
+++ 03 - Data Binding/4. Style/c. Multiple Style Objects/esbuild/js/class/App.jsx
@@ -0,0 +1,23 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import { Component } from 'react';
+
+class App extends Component {
+  render() {
+    const style1 = {
+      color: 'blue',
+      fontStyle: 'italic'
+    };
+    const style2 = {
+      color: 'red',
+      textDecoration: 'underline'
+    };
+
+    // Pure JS, nothing specific React.
+    // Some frameworks (like Vue) need special syntax.
+    return <h1 style={{ ...style1, ...style2 }}>Hello React!</h1>;
+  }
+}
+
+export default App;
