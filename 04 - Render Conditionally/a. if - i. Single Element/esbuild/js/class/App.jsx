cb17a65387230413b092ae3e27a8c3133f052898
Rick Beerendonk <rick@beerendonk.com>
2023-09-27 07:47:41 +0200

    Use ESBuild instead of SystemJS.

---
+++ 04 - Render Conditionally/a. if - i. Single Element/esbuild/js/class/App.jsx
@@ -0,0 +1,19 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import { Component } from 'react';
+
+class App extends Component {
+  render() {
+    const show = true;
+
+    return (
+      <>
+        <h3>Conditional:</h3>
+        {show && <div>Visible</div>}
+      </>
+    );
+  }
+}
+
+export default App;
