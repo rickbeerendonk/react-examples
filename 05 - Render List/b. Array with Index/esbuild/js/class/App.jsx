5bef65374020e64e9c7ded806cbfb3eeb96820d3
Rick Beerendonk <rick@beerendonk.com>
2023-09-27 15:36:05 +0200

    Use ESBuild instead of SystemJS.

---
+++ 05 - Render List/b. Array with Index/esbuild/js/class/App.jsx
@@ -0,0 +1,28 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import { Component } from 'react';
+
+class App extends Component {
+  render() {
+    const names = ['Alexandra', 'Benjamin', 'Charlotte'];
+
+    return (
+      <>
+        <h1>Greetings</h1>
+        <ol>
+          {
+            // Prefer map (instead of for-loops):
+            names.map((name, index) => (
+              <li key={name} value={index}>
+                Hello {name}
+              </li>
+            ))
+          }
+        </ol>
+      </>
+    );
+  }
+}
+
+export default App;
