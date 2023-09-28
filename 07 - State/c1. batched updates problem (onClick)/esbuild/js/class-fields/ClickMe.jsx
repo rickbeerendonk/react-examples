155e9ae75676443bfd6f79b5c47970b9a9c41798
Rick Beerendonk <rick@beerendonk.com>
2023-09-28 09:06:25 +0200

    Use ESBuild instead of SystemJS.

---
+++ 07 - State/c1. batched updates problem (onClick)/esbuild/js/class-fields/ClickMe.jsx
@@ -0,0 +1,26 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import { Component } from 'react';
+
+class ClickMe extends Component {
+  // Proposal: https://github.com/tc39/proposal-class-fields
+  // Support: http://kangax.github.io/compat-table/esnext/#test-instance_class_fields
+  state = { count: 0 };
+  handleClick = () => {
+    // Updates are batched for performance reasons.
+    // Therefore this won't work:
+    this.setState({ count: this.state.count + 0.4 });
+    this.setState({ count: this.state.count + 0.6 });
+  };
+
+  render() {
+    return (
+      <button onClick={this.handleClick}>
+        This link has been clicked {this.state.count} times
+      </button>
+    );
+  }
+}
+
+export default ClickMe;
---
+++ 07 - State/c1. batched updates problem (onClick)/esbuild/js/function/ClickMe.jsx
@@ -0,0 +1,23 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import { useState } from 'react';
+
+function ClickMe() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    // Updates are batched for performance reasons.
+    // Therefore this won't work:
+    setCount(count + 0.4);
+    setCount(count + 0.6);
+  }
+
+  return (
+    <button onClick={handleClick}>
+      This button has been clicked {count} times
+    </button>
+  );
+}
+
+export default ClickMe;
