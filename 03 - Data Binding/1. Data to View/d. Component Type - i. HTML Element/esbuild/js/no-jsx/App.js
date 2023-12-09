4f14f26633dcade5f212dfa69ed0982c2bd0b1de
Rick Beerendonk <rick@beerendonk.com>
2023-12-09 11:34:07 +0100

    Add no-jsx version.

---
+++ 03 - Data Binding/1. Data to View/d. Component Type - i. HTML Element/esbuild/js/no-jsx/App.js
@@ -0,0 +1,11 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import { createElement } from 'react';
+
+function App() {
+  const elem = 'h1';
+  return createElement(elem, undefined, 'Hello World!');
+}
+
+export default App;
