d829fdecded14d74254c0e33babb032d647af899
Rick Beerendonk <rick@beerendonk.com>
2023-12-10 14:35:31 +0100

    Remove unused imports.

--- 09 - Children/g2. Merge (flat array)/esbuild/js/function-destructuring/Greeting.jsx
+++ 09 - Children/g2. Merge (flat array)/esbuild/js/function-destructuring/Greeting.jsx
@@ -1,8 +1,6 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2018 Rick Beerendonk          !*/
 
-import React from 'react';
-
 function Greeting({ name }) {
   return <i>Hello {name}</i>;
 }
--- 09 - Children/g2. Merge (flat array)/esbuild/js/function-destructuring/SpecialList.jsx
+++ 09 - Children/g2. Merge (flat array)/esbuild/js/function-destructuring/SpecialList.jsx
@@ -1,8 +1,6 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2019 Rick Beerendonk          !*/
 
-import React from 'react';
-
 import Greeting from './Greeting.jsx';
 import List from './List.jsx';
 
