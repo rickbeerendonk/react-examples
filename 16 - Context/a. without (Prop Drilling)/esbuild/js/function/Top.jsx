54020a0a65b608aa36b9482b33c21dc252b05620
Rick Beerendonk <rick@beerendonk.com>
2023-12-10 15:58:48 +0100

    Remove unused imports.

--- 16 - Context/a. without (Prop Drilling)/esbuild/js/function/Top.jsx
+++ 16 - Context/a. without (Prop Drilling)/esbuild/js/function/Top.jsx
@@ -1,8 +1,6 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2018 Rick Beerendonk          !*/
 
-import React from 'react';
-
 import Middle from './Middle.jsx';
 
 function Top({ color }) {
