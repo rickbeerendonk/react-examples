65a4688b8558c9751fb614c64371744bdbbd84fa
Rick Beerendonk <rick@beerendonk.com>
2023-12-10 14:23:14 +0100

    Rename for valid URL.

--- 05 - Render List/z. (DO NOT USE) For-loop/esbuild/js/function/App.jsx
+++ 05 - Render List/z. (DO NOT USE) For-loop/esbuild/js/function/App.jsx
@@ -1,8 +1,6 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2019 Rick Beerendonk          !*/
 
-import React from 'react';
-
 function App() {
   const names = ['Alexandra', 'Benjamin', 'Charlotte'];
 
