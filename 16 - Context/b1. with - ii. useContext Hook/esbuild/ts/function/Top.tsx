fd4b3bfe83f306b635012433c394754d5177206e
Rick Beerendonk <rick@beerendonk.com>
2024-03-16 14:54:12 +0100

    Remove unused import.

--- 16 - Context/b1. with - ii. useContext Hook/esbuild/ts/function/Top.tsx
+++ 16 - Context/b1. with - ii. useContext Hook/esbuild/ts/function/Top.tsx
@@ -1,7 +1,7 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2018 Rick Beerendonk          !*/
 
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 import ColorContext from './ColorContext.tsx';
 import Middle from './Middle.tsx';
