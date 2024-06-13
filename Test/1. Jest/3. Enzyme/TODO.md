45eb78171d6cd5d7273ae501f3ced9e2b78ebeae
Rick Beerendonk <rick@beerendonk.com>
2024-06-13 09:54:12 +0200

    Remove Enzyme examples.

--- Test/1. Jest/3. Enzyme/TODO.md
+++
@@ -1,1 +0,0 @@
-https://enzymejs.github.io/enzyme/
--- Test/1. Jest/3. Enzyme/e. Context - ii. Provider/src/color-context.jsx
+++
@@ -1,8 +0,0 @@
-/*! European Union Public License version 1.2 !*/
-/*! Copyright © 2019 Rick Beerendonk          !*/
-
-import React from 'react';
-
-const ColorContext = React.createContext({ color: '#d676ff' });
-
-export default ColorContext;
