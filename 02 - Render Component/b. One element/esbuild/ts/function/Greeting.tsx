bfad35059908cd0af92d7643dfd60a2d443eec11
Rick Beerendonk <rick@beerendonk.com>
2024-08-17 10:48:05 +0200

    Remove unused import.

--- 02 - Render Component/b. One element/esbuild/ts/function/Greeting.tsx
+++ 02 - Render Component/b. One element/esbuild/ts/function/Greeting.tsx
@@ -1,8 +1,6 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2016 Rick Beerendonk          !*/
 
-import React from 'react';
-
 function Greeting() {
   return <h1>Hello World!</h1>;
 }
