01aaf1fa2d2510b111027dce352785f8ddc454d5
Rick Beerendonk <rick@beerendonk.com>
2024-08-23 00:46:56 +0200

    Add "Head - Double Title" example

---
+++ 99 - Future/a2. Head - Double Title/esm/js/jsx (React 19)/App.jsx
@@ -0,0 +1,14 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2024 Rick Beerendonk          !*/
+
+function App() {
+  return (
+    <>
+      <title>Title1</title>
+      <title>Title2</title>
+      <p>Observe that the title is Title2</p>
+    </>
+  );
+}
+
+export default App;
