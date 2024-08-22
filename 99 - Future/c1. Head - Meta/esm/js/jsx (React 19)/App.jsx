f944f4d2f1199e7918b7e7743e839c3e678a5b66
Rick Beerendonk <rick@beerendonk.com>
2024-08-23 00:58:01 +0200

    Add "Head - Meta" example.

---
+++ 99 - Future/c1. Head - Meta/esm/js/jsx (React 19)/App.jsx
@@ -0,0 +1,16 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2024 Rick Beerendonk          !*/
+
+function App() {
+  return (
+    <>
+      <h1>Meta element with description added</h1>
+      <p style={{ color: 'gray' }}>
+        Observe the description in the head section.
+      </p>
+      <meta name="description" content="This is a React 19 demo" />
+    </>
+  );
+}
+
+export default App;
