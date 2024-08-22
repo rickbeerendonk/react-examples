41b6e4f6f95b584e7b2a069b5c85525053bc2dfd
Rick Beerendonk <rick@beerendonk.com>
2024-08-23 00:56:18 +0200

    Add "Head - Link" example.

---
+++ 99 - Future/b1. Head - Link/esm/js/jsx (React 19)/App.jsx
@@ -0,0 +1,13 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2024 Rick Beerendonk          !*/
+
+function App() {
+  return (
+    <>
+      <h1>Title should be red (stylesheet added in component)</h1>
+      <link rel="stylesheet" href="App.css" />
+    </>
+  );
+}
+
+export default App;
