ecbd459c614cc1c82d8093c127cf84154d2c3a99
Rick Beerendonk <rick@beerendonk.com>
2024-12-03 18:31:47 +0100

    Delete all runtime examples.

--- 01 - Render Root/a. Concurrent Rendering (React 18)/i. Use existing DOM element/runtime/js/jsx/index.jsx
+++
@@ -1,7 +0,0 @@
-/*! European Union Public License version 1.2 !*/
-/*! Copyright © 2014 Rick Beerendonk          !*/
-
-/* global React, ReactDOM */
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<h1>Hello React {React.version}!</h1>);
