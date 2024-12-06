b0120f3e6a2f9d9640426f01186f35aa2bc8895e
Rick Beerendonk <rick@beerendonk.com>
2024-12-06 01:39:17 +0100

    Split file.

--- 99 - Future/f. useOptimistic [TODO]/esm/js/jsx/App.jsx
+++ 99 - Future/f. useOptimistic [TODO]/esm/js/jsx/App.jsx
@@ -3,34 +3,7 @@
 
 import { useOptimistic, useState, useRef } from 'react';
 
-function GreetingEditor({ name, sendName }) {
-  const formRef = useRef();
-  const [optimisticState, addOptimisticName] = useOptimistic(
-    name,
-    (state, newName) => {
-      return newName;
-    }
-  );
-
-  async function formAction(formData) {
-    const name = formData.get('name');
-    addOptimisticName(name);
-    sendName(name);
-    formRef.current.reset();
-  }
-
-  return (
-    <>
-      <form action={formAction} ref={formRef}>
-        <input type="name" name="name" placeholder="Name" />
-        <button type="submit">Send</button>
-      </form>
-      <p>
-        <b>Child name:</b> {optimisticState}
-      </p>
-    </>
-  );
-}
+import GreetingEditor from './GreetingEditor.jsx';
 
 function App() {
   const [name, setName] = useState('World');
