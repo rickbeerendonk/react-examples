a846311a2b05aed8c0ca140a9bd774e891945a62
Rick Beerendonk <rick@beerendonk.com>
2025-03-02 09:51:56 +0100

    Simplification.

--- 03 - Data Binding/3. Class/a. ClassName/esbuild/js/App.jsx
+++ 03 - Data Binding/3. Class/a. ClassName/esbuild/js/App.jsx
@@ -1,8 +1,6 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2021 Rick Beerendonk          !*/
 
-const App = () => {
+export default function App() {
   return <h1 className="first second">Hello React!</h1>;
-};
-
-export default App;
+}
