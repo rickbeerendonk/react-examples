68ace8caf68d50571bc43f6ed36ed2ed91f967d3
Rick Beerendonk <rick@beerendonk.com>
2025-03-04 11:19:05 +0100

    Fix.

--- 07 - State/a. fixed updater (onChange)/esbuild/js/GreetingEditor.jsx
+++ 07 - State/a. fixed updater (onChange)/esbuild/js/GreetingEditor.jsx
@@ -5,24 +5,15 @@ import { useState } from 'react';
 
 function GreetingEditor() {
   const [name, setName] = useState('World');
-  const [count, setCount] = useState(0);
-
-  let cities = ['Oslo', 'Berg'];
 
   function handleChange(e) {
     setName(e.target.value);
-  }
-
-  function handleClick() {
-    setCount(c => c + 0.4);
-    setCount(c => c + 0.6);
   }
 
   return (
     <>
       <input onChange={handleChange} value={name} />
       <h1>Hello {name}!</h1>
-      <button onClick={handleClick}>I have been clicked {count} times</button>
     </>
   );
 }
