e215667582413ef82fa53656cec46bce20f1027d
Rick Beerendonk <rick@beerendonk.com>
2025-03-05 22:15:33 +0100

    Direct export.

--- 08 - Multiple Components/a. Basics/esbuild/js/EditBox.jsx
+++ 08 - Multiple Components/a. Basics/esbuild/js/EditBox.jsx
@@ -1,12 +1,10 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2018-2024 Rick Beerendonk     !*/
 
-function EditBox({ onChange, value }) {
+export default function EditBox({ onChange, value }) {
   function handleChange(e) {
     onChange(e.target.value);
   }
 
   return <input onChange={handleChange} value={value} />;
 }
-
-export default EditBox;
