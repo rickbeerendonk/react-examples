31c7831a605c6e77f99b31bf9bfee961991fa4fa
Rick Beerendonk <rick@beerendonk.com>
2025-03-05 22:16:01 +0100

    Direct export.

--- Debugging/a. Basics/esbuild/js/props/Greeting.jsx
+++ Debugging/a. Basics/esbuild/js/props/Greeting.jsx
@@ -1,8 +1,6 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2017 Rick Beerendonk          !*/
 
-function Greeting({ name }) {
+export default function Greeting({ name }) {
   return <h1>Hello {name}!</h1>;
 }
-
-export default Greeting;
