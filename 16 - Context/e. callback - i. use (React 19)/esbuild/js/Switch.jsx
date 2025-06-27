514dc3db10713803d32615ac98848c0432aac5b3
Rick Beerendonk <rick@beerendonk.com>
2025-06-27 09:40:27 +0200

    Reorder.

---
+++ 16 - Context/e. callback - i. use (React 19)/esbuild/js/Switch.jsx
@@ -0,0 +1,18 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import { use } from 'react';
+
+import ColorContext from './ColorContext.js';
+
+function Switch() {
+  const { handleSwitch } = use(ColorContext);
+
+  function handleClick() {
+    handleSwitch();
+  }
+
+  return <button onClick={handleClick}>Switch color</button>;
+}
+
+export default Switch;
---
+++ 16 - Context/e. callback - i. use (React 19)/esbuild/ts/Middle.tsx
@@ -0,0 +1,16 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import Switch from './Switch.tsx';
+import Bottom from './Bottom.tsx';
+
+function Middle() {
+  return (
+    <>
+      <Bottom />
+      <Switch />
+    </>
+  );
+}
+
+export default Middle;
