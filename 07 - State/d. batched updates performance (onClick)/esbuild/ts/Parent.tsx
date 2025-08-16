7a8eaab134038fffb9f87cfd41104c61063c2e9c
Rick Beerendonk <rick@beerendonk.com>
2025-08-16 18:53:23 +0200

    Add TS version.

---
+++ 07 - State/d. batched updates performance (onClick)/esbuild/ts/Parent.tsx
@@ -0,0 +1,19 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import { useState } from 'react';
+
+import Child from './Child.tsx';
+
+function Parent() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    // Batched:
+    setCount(c => c + 1);
+  }
+
+  return <Child count={count} onClick={handleClick} />;
+}
+
+export default Parent;
---
+++ 07 - State/d. batched updates performance (onClick)/esbuild/ts/main.tsx
@@ -0,0 +1,9 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import { createRoot } from 'react-dom/client';
+
+import Parent from './Parent.tsx';
+
+const root = createRoot(document.getElementById('root'));
+root.render(<Parent />);
