6e3cea6b376d6ad24cc2207114401206b1e3fdd3
Rick Beerendonk <rick@beerendonk.com>
2025-08-22 09:43:09 +0200

    Add TS version.

---
+++ 12 - Hooks/f. Custom - i. useBoolean/esbuild/ts/hooks/boolean-hook.ts
@@ -0,0 +1,24 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import { useCallback, useDebugValue, useState } from 'react';
+
+function useBoolean(initialValue: boolean): [boolean, () => void] {
+  if (typeof initialValue !== 'boolean') {
+    throw new Error('Initial value must be a boolean');
+  }
+
+  const [value, setValue] = useState(initialValue);
+
+  const toggle = useCallback(() => {
+    setValue(v => !v);
+  }, []);
+
+  // DevTools state
+  useDebugValue(value);
+
+  // Return current state & toggle function
+  return [value, toggle];
+}
+
+export default useBoolean;
---
+++ 12 - Hooks/f. Custom - i. useBoolean/esbuild/ts/main.tsx
@@ -0,0 +1,9 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+import { createRoot } from 'react-dom/client';
+
+import App from './App.tsx';
+
+const root = createRoot(document.getElementById('root')!);
+root.render(<App />);
