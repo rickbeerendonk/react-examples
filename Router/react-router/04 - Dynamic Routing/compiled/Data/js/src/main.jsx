2e691c0441b93c83b8a66640f80afd0502d8ad5a
Rick Beerendonk <rick@beerendonk.com>
2025-08-22 14:19:48 +0200

    Add compiled version.

---
+++ Router/react-router/04 - Dynamic Routing/compiled/Data/js/src/main.jsx
@@ -0,0 +1,30 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019-2024 Rick Beerendonk !*/
+
+import { createRoot } from 'react-dom/client';
+import { createHashRouter, RouterProvider } from 'react-router-dom';
+import './index.css';
+
+import Root from './Root.jsx';
+import Home from './routes/Home.jsx';
+import Page from './routes/Page.jsx';
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: 'page/:id',
+        element: <Page />
+      }
+    ]
+  }
+]);
+
+const root = createRoot(document.getElementById('root'));
+root.render(<RouterProvider router={router} />);
