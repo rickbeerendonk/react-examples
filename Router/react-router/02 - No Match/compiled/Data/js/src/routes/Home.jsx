19b3d20b5c5fcc14a234a60896a3fa6691ff073a
Rick Beerendonk <rick@beerendonk.com>
2025-08-22 14:15:21 +0200

    Add compiled version.

---
+++ Router/react-router/02 - No Match/compiled/Data/js/src/routes/Home.jsx
@@ -0,0 +1,13 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2018 Rick Beerendonk          !*/
+
+function Home() {
+  return (
+    <>
+      <title>Home</title>
+      <h2>Home</h2>
+    </>
+  );
+}
+
+export default Home;
