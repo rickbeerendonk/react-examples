b3ca405d295b0d8ac99eb8872ecd814e5a2a6427
Rick Beerendonk <rick@beerendonk.com>
2025-08-22 14:17:35 +0200

    Add compiled version.

---
+++ Router/react-router/03 - Redirect/compiled/Data/js/src/main.jsx
@@ -0,0 +1,40 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019-2024 Rick Beerendonk !*/
+
+import { createRoot } from 'react-dom/client';
+import { createHashRouter, RouterProvider, Navigate } from 'react-router-dom';
+import './index.css';
+
+import Root from './Root.jsx';
+import Home from './routes/Home.jsx';
+import Page1 from './routes/Page1.jsx';
+import Page2 from './routes/Page2.jsx';
+
+const router = createHashRouter([
+  {
+    path: '/',
+    element: <Root />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+      {
+        path: 'page1',
+        element: <Page1 />
+      },
+      {
+        path: 'page2',
+        element: <Page2 />
+      },
+      {
+        path: 'oldpage2/*',
+        // Prefer handling this on the server with HTTP code 301 (or 308)
+        element: <Navigate replace to="/page2" />
+      }
+    ]
+  }
+]);
+
+const root = createRoot(document.getElementById('root'));
+root.render(<RouterProvider router={router} />);
