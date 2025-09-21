0ae3471432ced78ba7f425d8115699963255c2c1
Rick Beerendonk <rick@beerendonk.com>
2025-09-22 00:33:31 +0200

    Move example to correct location (but leave in React 19 for now).

---
+++ 12 - Hooks/i. useTransition - iv. Async (React 19)/esbuild/js/App.jsx
@@ -0,0 +1,25 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2023 Rick Beerendonk          !*/
+
+import { useState, useTransition } from 'react';
+
+import { slowAddAsync } from './utils/index.js';
+
+export default function App() {
+  const [count, setCount] = useState(0);
+  const [isPending, startTransition] = useTransition();
+
+  function handleClick() {
+    // Async function supported since React 19
+    startTransition(async () => {
+      const newCount = await slowAddAsync(count);
+      setCount(newCount);
+    });
+  }
+
+  return (
+    <button onClick={handleClick}>
+      Change count {isPending ? '[calculating...]' : count}
+    </button>
+  );
+}
