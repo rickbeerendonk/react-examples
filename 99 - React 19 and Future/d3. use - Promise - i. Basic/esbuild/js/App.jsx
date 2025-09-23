b88551f6de660e34230a59a7765be38c7bfe5e74
Rick Beerendonk <rick@beerendonk.com>
2025-09-23 22:25:13 +0200

    One component per file.

--- 99 - React 19 and Future/d3. use - Promise - i. Basic/esbuild/js/App.jsx
+++ 99 - React 19 and Future/d3. use - Promise - i. Basic/esbuild/js/App.jsx
@@ -1,22 +1,9 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2024 Rick Beerendonk          !*/
 
-import { Suspense, use } from 'react';
+import { Suspense } from 'react';
 
-function valueFunc() {
-  return new Promise(resolve => {
-    setTimeout(() => {
-      resolve('Hello, world!');
-    }, 2000);
-  });
-}
-
-const valuePromise = valueFunc();
-
-function Value() {
-  const value = use(valuePromise);
-  return <h1>{value}</h1>;
-}
+import Value from './Value.jsx';
 
 export default function App() {
   return (
