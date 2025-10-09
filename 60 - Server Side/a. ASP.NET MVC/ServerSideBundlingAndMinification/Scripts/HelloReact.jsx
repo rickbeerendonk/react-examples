b5edd02dbc23ca8de04da4ef7d48346438f4d334
Rick Beerendonk <rick@beerendonk.com>
2025-10-10 00:52:24 +0200

    Remove unpkg dependencies.

--- 60 - Server Side/a. ASP.NET MVC/ServerSideBundlingAndMinification/Scripts/HelloReact.jsx
+++
@@ -1,9 +0,0 @@
-﻿/* global React, ReactDOM */
-
-class HelloReact extends React.Component {
-  render() {
-    return <h1>Hello React!</h1>;
-  }
-}
-
-ReactDOM.createRoot(document.getElementById('root')).render(<HelloReact />);
