fdb37e629d4fe33ee0d32cfb7f4b9dab568a7217
Rick Beerendonk <rick@beerendonk.com>
2025-12-07 01:14:35 +0100

    Add typescript types.

--- utils/slow-fetch.d.ts
+++ utils/slow-fetch.d.ts
@@ -1,4 +1,10 @@
-export function fetch(
-  input: RequestInfo | URL,
-  init?: RequestInit
-): Promise<Response>;
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2025 Rick Beerendonk          !*/
+
+declare module 'slow-fetch' {
+  export function slowfetch(
+    ms: number,
+    ...args: Parameters<typeof fetch>
+  ): Promise<Response>;
+  export function fetch(...args: Parameters<typeof fetch>): Promise<Response>;
+}
