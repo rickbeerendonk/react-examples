769501b6704221540b7f392d689baa503f85021e
Rick Beerendonk <rick@beerendonk.com>
2026-01-07 23:08:28 +0100

    Use Vite.

---
+++ 03 - Data Binding/1. Data to View/d. No JSX/js/src/main.jsx
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2021 Rick Beerendonk          !*/
+
+import { createRoot } from 'react-dom/client';
+
+import App from './App.js';
+import './oblicum.css';
+
+const root = createRoot(document.getElementById('root'));
+root.render(<App />);
