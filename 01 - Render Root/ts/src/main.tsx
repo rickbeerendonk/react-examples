94c384bbb0cb637259eb20ea3898685d76b520e9
Rick Beerendonk <rick@beerendonk.com>
2026-01-07 22:22:55 +0100

    Use oblicum.css as name.

--- 01 - Render Root/ts/src/main.tsx
+++ 01 - Render Root/ts/src/main.tsx
@@ -3,7 +3,7 @@
 
 import { version } from 'react';
 import { createRoot } from 'react-dom/client';
-import './index.css';
+import './oblicum.css';
 
 const root = createRoot(document.getElementById('root')!);
 root.render(<h1>Hello React {version}!</h1>);
