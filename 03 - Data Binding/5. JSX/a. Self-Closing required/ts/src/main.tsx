fe1ee967c00ee05ece735534cef12dff2c670681
Rick Beerendonk <rick@beerendonk.com>
2026-01-07 23:44:07 +0100

    Use Vite.

--- 03 - Data Binding/5. JSX/a. Self-Closing required/ts/src/main.tsx
+++ 03 - Data Binding/5. JSX/a. Self-Closing required/ts/src/main.tsx
@@ -4,6 +4,7 @@
 import { createRoot } from 'react-dom/client';
 
 import App from './App.tsx';
+import './oblicum.css';
 
 const root = createRoot(document.getElementById('root')!);
 root.render(<App />);
