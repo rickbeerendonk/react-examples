d1004c7441b39c6ebcbaea686e4664a6d0d8e9f6
Rick Beerendonk <rick@beerendonk.com>
2026-01-08 00:39:46 +0100

    Use Vite.

---
+++ 21 - Performance Optimization/b. Runtime Performance/a2. useCallback/ts/src/main.tsx
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+import { createRoot } from 'react-dom/client';
+
+import Parent from './Parent.tsx';
+import './oblicum.css';
+
+const root = createRoot(document.getElementById('root') as Element);
+root.render(<Parent />);
