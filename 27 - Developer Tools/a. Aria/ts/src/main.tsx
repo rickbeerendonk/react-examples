b8a1d8be6d12638440359fb53af037a33c5734f8
Rick Beerendonk <rick@beerendonk.com>
2026-01-09 23:44:59 +0100

    Use Vite.

---
+++ 27 - Developer Tools/a. Aria/ts/src/main.tsx
@@ -0,0 +1,10 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2025 Rick Beerendonk          !*/
+
+import { createRoot } from 'react-dom/client';
+
+import App from './App.tsx';
+import './oblicum.css';
+
+const root = createRoot(document.getElementById('root')!);
+root.render(<App />);
