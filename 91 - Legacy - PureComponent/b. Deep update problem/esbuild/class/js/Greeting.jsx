d118871f8241fd8d2af677286e29f9bd459392ea
Rick Beerendonk <rick@beerendonk.com>
2026-01-10 00:51:50 +0100

    Use Vite.

--- 91 - Legacy - PureComponent/b. Deep update problem/esbuild/class/js/Greeting.jsx
+++
@@ -1,9 +0,0 @@
-/*! European Union Public License version 1.2 !*/
-/*! Copyright © 2018 Rick Beerendonk          !*/
-
-/* eslint react/prop-types:"off", no-console:"off" */
-
-// Greeting is NOT a pure component:
-const Greeting = ({ name }) => (name ? `Hello ${name}!` : null);
-
-export default Greeting;
