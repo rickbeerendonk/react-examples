2af754f9e677daad62d9366350d116bc76e4a3cf
Rick Beerendonk <rick@beerendonk.com>
2026-01-10 19:05:59 +0100

    Use Vite.

--- 41 - Pattern - Props and State Destructuring in render()/class/src/main.jsx
+++ 41 - Pattern - Props and State Destructuring in render()/class/src/main.jsx
@@ -1,7 +1,9 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2018 Rick Beerendonk          !*/
 
+import { version } from 'react';
 import { createRoot } from 'react-dom/client';
+import './oblicum.css';
 
 import GreetingEditor from './GreetingEditor.jsx';
 
