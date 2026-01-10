9f0f4892d540e82dc5440278751309540fef5efc
Rick Beerendonk <rick@beerendonk.com>
2026-01-10 01:36:58 +0100

    Use Vite.

---
+++ 47 - Pattern - Flexible Compound Component/b. Error when rendered outside parent/js/src/main.jsx
@@ -0,0 +1,15 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2023 Rick Beerendonk          !*/
+
+import { createRoot } from 'react-dom/client';
+
+import GreetingEditor from './GreetingEditor.jsx';
+import './oblicum.css';
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <>
+    <GreetingEditor.Text />
+    <GreetingEditor.Input />
+  </>
+);
---
+++ 47 - Pattern - Flexible Compound Component/e. Limit Children/js/src/main.jsx
@@ -0,0 +1,18 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2020 Rick Beerendonk          !*/
+
+import { createRoot } from 'react-dom/client';
+
+import CustomContent from './CustomContent.jsx';
+import GreetingEditor from './GreetingEditor.jsx';
+import './oblicum.css';
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+  <GreetingEditor>
+    <CustomContent />
+
+    <GreetingEditor.Input />
+    <GreetingEditor.Text />
+  </GreetingEditor>
+);
