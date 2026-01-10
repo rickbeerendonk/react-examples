89d19ffea34a1f082298cbc026f64c0a84e14a85
Rick Beerendonk <rick@beerendonk.com>
2026-01-10 01:00:15 +0100

    Use Vite.

--- 07 - State/d. Batched Updates/3. Performance Benefit/esbuild/ts/main.tsx
+++
@@ -1,9 +0,0 @@
-/*! European Union Public License version 1.2 !*/
-/*! Copyright © 2018 Rick Beerendonk          !*/
-
-import { createRoot } from 'react-dom/client';
-
-import Parent from './Parent.tsx';
-
-const root = createRoot(document.getElementById('root'));
-root.render(<Parent />);
