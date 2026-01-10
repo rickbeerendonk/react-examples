1c475630d9683ebdfe373ea90f802ecda09e4531
Rick Beerendonk <rick@beerendonk.com>
2026-01-10 16:32:01 +0100

    Use Vite.

--- Debugging/a. Basics/esbuild/class/js/props/Greeting.jsx
+++
@@ -1,8 +0,0 @@
-/*! European Union Public License version 1.2 !*/
-/*! Copyright © 2017 Rick Beerendonk          !*/
-
-function Greeting({ name }) {
-  return <h2>Hello {name}!</h2>;
-}
-
-export default Greeting;
