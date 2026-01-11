29e42ad400c337ba1458dcc2b3a9bbc9b9ffa99c
Rick Beerendonk <rick@beerendonk.com>
2026-01-12 00:37:30 +0100

    Use Vite.

---
+++ 70 - Strict Mode/b1. Javascript URL/js/src/Dangerous.jsx
@@ -0,0 +1,19 @@
+/*! European Union Public License version 1.2 !*/
+/*! Copyright © 2019 Rick Beerendonk          !*/
+
+function Dangerous() {
+  // Pull request: https://github.com/facebook/react/pull/15047
+  /*
+    Dangerous:
+    <a href />
+    <form action />
+    <iframe src />
+    <area href />
+    <button formaction />
+    <input formaction />
+    <frame src />
+  */
+  return <a href="javascript:alert('Is impossible.')">Dangerous</a>;
+}
+
+export default Dangerous;
