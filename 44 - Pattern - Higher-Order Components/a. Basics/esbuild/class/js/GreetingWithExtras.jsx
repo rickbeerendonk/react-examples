887212c2fa6befef44330fdc77efe2729080b49e
Rick Beerendonk <rick@beerendonk.com>
2026-01-12 00:16:14 +0100

    Use Vite.

--- 44 - Pattern - Higher-Order Components/a. Basics/esbuild/class/js/GreetingWithExtras.jsx
+++
@@ -1,9 +0,0 @@
-/*! European Union Public License version 1.2 !*/
-/*! Copyright © 2018 Rick Beerendonk          !*/
-
-import Greeting from './Greeting.jsx';
-import { withExtras } from './hoc.jsx';
-
-const GreetingWithExtras = withExtras(Greeting);
-
-export default GreetingWithExtras;
