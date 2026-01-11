d0dfb045f5b4c725f8475f6867a97e4abd33f852
Rick Beerendonk <rick@beerendonk.com>
2026-01-12 00:48:48 +0100

    Remove unused resources.

--- resources/path-to-regexp.mjs
+++
@@ -1,9 +0,0 @@
-/*! European Union Public License version 1.2 !*/
-/*! Copyright © 2018-2023 Rick Beerendonk     !*/
-
-/* global pathToRegexp */
-
-export default pathToRegexp;
-
-export const { parse, compile, tokensToFunction, tokensToRegExp } =
-  pathToRegexp;
--- resources/react-jsx-runtime-next.development.mjs
+++
@@ -1,8 +0,0 @@
-/*! European Union Public License version 1.2 !*/
-/*! Copyright © 2023 Rick Beerendonk          !*/
-
-/* global ReactJSXRuntime */
-
-export default ReactJSXRuntime;
-
-export const { Fragment, jsx, jsxs } = ReactJSXRuntime;
