f251b14e663a9b94dfedc1b86a13f5b903102513
Rick Beerendonk <rick@beerendonk.com>
2026-01-14 02:02:02 +0100

    Enable sourcemaps.

--- 51 - Dynamic Components/b. Lazy Loading/js/vite.config.js
+++ 51 - Dynamic Components/b. Lazy Loading/js/vite.config.js
@@ -4,5 +4,11 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: { port: 9100 }
+  server: { port: 9100 },
+  build: {
+    sourcemap: true
+  },
+  define: {
+    'process.env.NODE_ENV': '"development"'
+  }
 });
