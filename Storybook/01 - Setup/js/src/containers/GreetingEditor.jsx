4283b153f1209b2cf8560262b97b844293cd0a0d
Rick Beerendonk <rick@beerendonk.com>
2026-02-18 01:18:28 +0100

    Remove unused code.

--- Storybook/01 - Setup/js/src/containers/GreetingEditor.jsx
+++ Storybook/01 - Setup/js/src/containers/GreetingEditor.jsx
@@ -1,7 +1,7 @@
 /*! European Union Public License version 1.2 !*/
 /*! Copyright © 2017 Rick Beerendonk          !*/
 
-import React, { useState } from 'react';
+import { useState } from 'react';
 
 import EditBox from '../components/EditBox';
 import Greeting from '../components/Greeting';
