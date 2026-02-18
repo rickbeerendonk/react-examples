f17e1a7e495b8d8e4ceeaef7f41ec9c115ef3f97
Rick Beerendonk <rick@beerendonk.com>
2026-02-18 01:18:49 +0100

    Ignore Storybook 7+ folders.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -10,7 +10,7 @@ export default [
   {
     ignores: [
       '** /build/ **',
-      '**/build-storybook/**',
+      '**/storybook-static/**',
       '**/coverage/**',
       '**/lib/**',
       '**/node_modules/**',
