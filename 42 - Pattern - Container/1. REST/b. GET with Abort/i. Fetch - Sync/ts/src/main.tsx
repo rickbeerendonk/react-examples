5ad0f6eed3cb8d1da12528b2b9cd29c978098187
Rick Beerendonk <rick@beerendonk.com>
2026-02-19 11:28:43 +0100

    Fix example.

--- 42 - Pattern - Container/1. REST/b. GET with Abort/i. Fetch - Sync/ts/src/main.tsx
+++
@@ -1,9 +0,0 @@
-/*! European Union Public License version 1.2 !*/
-/*! Copyright © 2014 Rick Beerendonk          !*/
-
-import { version } from 'react';
-import { createRoot } from 'react-dom/client';
-import './oblicum.css';
-
-const root = createRoot(document.getElementById('root')!);
-root.render(<h1>Hello React {version}!</h1>);
